dcd57fc84692f6166ff66f6d90e6f8c906811922
hilde.heggstad <hilde.heggstad@bekk.no>
2019-02-15 13:42:19 +0100

    endret package json

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,53 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Input} from "nav-frontend-skjema";
+import {Sidetittel} from "nav-frontend-typografi";
+import Veilederpanel from 'nav-frontend-veilederpanel';
+import Veileder from 'nav-frontend-veileder';
+import navkopp from './navkopp.png';
+import PanelBase from 'nav-frontend-paneler';
+import { Select } from 'nav-frontend-skjema';
+import Ekspanderbartpanel from 'nav-frontend-ekspanderbartpanel';
+
+
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-      <h1>Velkommen til ditt nav for arbeidsgivere</h1>
-        </header>
-      </div>
+
+        <div className = "forside">
+            <PanelBase className = "overpanel">
+                <Select className = "menu1" label=''>
+                    <option value=''>Language</option>
+                    <option value='norge'>Norge</option>
+                    <option value='sverige'>Sverige</option>
+                    <option value='danmark'>Danmark</option>
+                </Select>
+                <Select className="menu2" label='Høykontrast'>
+                    <option value=''>Velg land</option>
+                    <option value='norge'>Norge</option>
+                    <option value='sverige'>Sverige</option>
+                    <option value='danmark'>Danmark</option>
+                </Select>
+                <Select className="menu3" label='Skriftstørrelse'>
+                    <option value=''>Velg land</option>
+                    <option value='norge'>Norge</option>
+                    <option value='sverige'>Sverige</option>
+                    <option value='danmark'>Danmark</option>
+                </Select>
+            </PanelBase>
+          <Sidetittel>Ditt NAV for arbeidsgivere</Sidetittel>
+        <Input label=' ID '/>
+          <Input label=' Passord '/>
+        </div>
+
+
+
+
+
+
     );
   }
 }
