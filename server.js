9f590f980a131184527958db0adc0690d6ec6b64
Bendik Segrov Ibenholt <bendik.ibenholt@bekk.no>
2019-02-27 15:44:34 +0100

    koreksjon server.js require

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ server.use('/ditt-nav-arbeidsgiver', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
 });
 
-server.use(('/api'), sonekrysning);
+server.use('/api', sonekrysning);
 
 server.listen(port, () => {
     console.log('Server listening on port', port);
