b3ec62f4c4fe22d0d2dadd4be4e32da562d24190
Bendik Segrov Ibenholt <bendik.ibenholt@bekk.no>
2019-02-27 14:36:01 +0100

    la til proxy for sonekryssning

---
+++ src/api/apiPathConfig.js
@@ -0,0 +1,25 @@
+const proxy = require('http-proxy-middleware');
+const basePath = require('./basePath');
+
+const envProperties = {
+    API_GATEWAY: process.env.API_GATEWAY || 'http://localhost:8080',
+    APIGW_HEADER: process.env.APIGW_HEADER,
+};
+
+const proxyConfig = {
+    changeOrigin: true,
+    pathRewrite: {
+        ['^' + basePath('/api')]: `/organisasjoner`,
+    },
+    target: envProperties.API_GATEWAY,
+    secure: true,
+    xfwd: true,
+};
+
+if (envProperties.APIGW_HEADER) {
+    proxyConfig.headers = {
+        'x-nav-apiKey': envProperties.APIGW_HEADER,
+    };
+}
+
+module.exports = proxy(proxyConfig);
--- src/api/apiPathConfig.ts
+++
@@ -1,4 +0,0 @@
-export const envProperties = {
-    API_GATEWAY: process.env.API_GATEWAY || 'http://localhost:8080',
-    APIGW_HEADER: process.env.APIGW_HEADER,
-}
