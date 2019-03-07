8ab2ca453ce4e9466c1dac3f7821c9dffe00a254
André Mossige <mossige.a@gmail.com>
2019-03-07 16:29:44 +0100

    Fiks at app ikke krasjer hvis feil fra API

--- src/api/dnaApi.ts
+++ src/api/dnaApi.ts
@@ -1,15 +1,10 @@
 import { Organisasjon } from "../organisasjon";
 
-export async function hentHello(): Promise<string> {
-  let respons = await fetch("/ditt-nav-arbeidsgiver/api");
-  let tekst = await respons.text();
-  return tekst;
-}
-
 export async function hentOrganisasjoner(): Promise<Array<Organisasjon>> {
   let respons = await fetch("/ditt-nav-arbeidsgiver/api/organisasjoner");
-  console.log(respons);
-  let organisasjonsArray = await respons.json();
-  console.log(organisasjonsArray);
-  return organisasjonsArray;
+  if (respons.ok) {
+    return await respons.json();
+  } else {
+    return [];
+  }
 }
