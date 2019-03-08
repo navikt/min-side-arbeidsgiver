d4a9c956f710fbff8e4befa0ee2196a22ebb01e2
hilde.heggstad <hilde.heggstad@bekk.no>
2019-03-08 12:52:20 +0100

    redirect hvis uauthorized

--- src/api/dnaApi.ts
+++ src/api/dnaApi.ts
@@ -2,9 +2,17 @@ import { Organisasjon } from "../organisasjon";
 
 export async function hentOrganisasjoner(): Promise<Array<Organisasjon>> {
   let respons = await fetch("/ditt-nav-arbeidsgiver/api/organisasjoner");
+  redirectHvisUnauthorized(respons);
   if (respons.ok) {
     return await respons.json();
   } else {
     return [];
+  }
+}
+
+function redirectHvisUnauthorized(respons: Response) {
+  if (respons.status === 401) {
+    window.location.href =
+      "http://localhost:8080/ditt-nav-arbeidsgiver-api/local/selvbetjening-login?redirect=http://localhost:3000/ditt-nav-arbeidsgiver";
   }
 }
