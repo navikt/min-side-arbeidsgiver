725a143aa9e774413ae0098c13871e50b47dc1f7
hilde.heggstad <hilde.heggstad@bekk.no>
2019-03-18 16:40:47 +0100

    lagt til styling på kontakt navboksen

--- src/App/Hovedside/ArbeidsgiverTelefon/ArbeidsgiverTelefon.tsx
+++ src/App/Hovedside/ArbeidsgiverTelefon/ArbeidsgiverTelefon.tsx
@@ -10,10 +10,13 @@ const ArbeidsgiverTelefon: FunctionComponent = () => {
     <Innholdsboks>
       <div className={"arbeidsgivertelefon"}>
         <Undertittel>{"Arbeidsgivertelefonen"}</Undertittel>
-        <div className={"containerinnhold"}>
-          <img className={"icontlf"} src={iconTlf} />
-          <div className={"containerNrogTidspunkt"}>
-            <Lenke className={"linknummer"} href="tel:55-55-33-36">
+        <div className={"arbeidsgivertelefon__innhold"}>
+          <img className={"arbeidsgivertelefon__icon"} src={iconTlf} />
+          <div className={"arbeidsgivertelefon__nrogTidspunkt"}>
+            <Lenke
+              className={"arbeidsgivertelefon__nummer"}
+              href="tel:55-55-33-36"
+            >
               55 55 33 36
             </Lenke>
             <Normaltekst>{"Kl 08.00 - 15.30 (hverdager)"}</Normaltekst>
