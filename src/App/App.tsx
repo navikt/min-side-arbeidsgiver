145be3f68df1d6c8453decd96c334f43c918f27a
hilde.heggstad <hilde.heggstad@bekk.no>
2019-03-20 14:38:06 +0100

    lagd organisasjonsprovider med context

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { FunctionComponent, useContext, useEffect } from "react";
 import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 import "./App.less";
 import { Organisasjon } from "../organisasjon";
@@ -8,49 +8,26 @@ import Hovedside from "./Hovedside/Hovedside";
 import Banner from "./Banner/Banner";
 import { hentOrganisasjoner } from "../api/dnaApi";
 import LoginBoundary from "./LoginBoundary";
+import {
+  OrganisasjonContext,
+  OrganisasjonProvider
+} from "../OrganisasjonProvider";
 
-interface State {
-  organisasjoner: Array<Organisasjon>;
-  valgtOrganisasjon?: Organisasjon;
-}
-class App extends Component<{}, State> {
-  state: State = {
-    organisasjoner: [],
-    valgtOrganisasjon: undefined
-  };
-
-  endreOrganisasjon = (orgnr: string) => {
-    const valgtOrganisasjon = this.state.organisasjoner.find(
-      org => orgnr === org.OrganizationNumber
-    );
-
-    this.setState({ valgtOrganisasjon: valgtOrganisasjon });
-  };
-
-  async componentDidMount() {
-    let organisasjoner = await hentOrganisasjoner();
-    this.setState({ organisasjoner });
-  }
-
-  render() {
-    return (
-      <div className={"bakgrunnsside"}>
-        <Banner
-          tittel={"Ditt nav arbeidsgiver"}
-          organisasjoner={this.state.organisasjoner}
-          endreOrganisasjon={this.endreOrganisasjon}
-          organisasjon={this.state.valgtOrganisasjon}
-        />
-        <LoginBoundary>
+const App: FunctionComponent = () => {
+  return (
+    <LoginBoundary>
+      <OrganisasjonProvider>
+        <div className={"bakgrunnsside"}>
+          <Banner tittel={"Ditt nav arbeidsgiver"} />
           <BrowserRouter basename={basename}>
             <Switch>
               <Route path="/" exact={true} component={Hovedside} />
             </Switch>
           </BrowserRouter>
-        </LoginBoundary>
-      </div>
-    );
-  }
-}
+        </div>
+      </OrganisasjonProvider>
+    </LoginBoundary>
+  );
+};
 
 export default App;
