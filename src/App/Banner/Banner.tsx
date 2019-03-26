5ce8a087d444640bd0939166b7d67bd9cf49ba27
hilde.heggstad <hilde.heggstad@bekk.no>
2019-03-26 13:51:45 +0100

    endret utseende på dropdownmenyen

--- src/App/Banner/Banner.tsx
+++ src/App/Banner/Banner.tsx
@@ -4,35 +4,44 @@ import "./Banner.less";
 import companyImage from "./company.svg";
 import { Select } from "nav-frontend-skjema";
 import { OrganisasjonContext } from "../../OrganisasjonProvider";
+import { Normaltekst } from "nav-frontend-typografi";
 
 interface Props {
   tittel: string;
 }
 
 const Banner: FunctionComponent<Props> = props => {
-  const { organisasjoner, endreOrganisasjon } = useContext(OrganisasjonContext);
+  const { organisasjoner, endreOrganisasjon, valgtOrganisasjon } = useContext(
+    OrganisasjonContext
+  );
 
   return (
     <div className={"banner"}>
       <img src={companyImage} />
       <Sidetittel className={"banner__tittel"}> {props.tittel} </Sidetittel>
       {organisasjoner.length > 0 && (
-        <Select
-          className={"banner__organisasjoner"}
-          label=""
-          onChange={event => endreOrganisasjon(event.target.value)}
-        >
-          {organisasjoner.map(organisasjon => (
-            <option
-              key={organisasjon.OrganizationNumber}
-              value={organisasjon.OrganizationNumber}
-            >
-              {`${organisasjon.Name} org.nr: ${
-                organisasjon.OrganizationNumber
-              }`}
-            </option>
-          ))}
-        </Select>
+        <div className={"banner__select"}>
+          <Select
+            className={"banner__organisasjoner"}
+            label={""}
+            onChange={event => endreOrganisasjon(event.target.value)}
+          >
+            {organisasjoner.map(organisasjon => (
+              <option
+                className={"banner__option"}
+                key={organisasjon.OrganizationNumber}
+                value={organisasjon.OrganizationNumber}
+              >
+                {organisasjon.Name}
+              </option>
+            ))}
+          </Select>
+          {valgtOrganisasjon && (
+            <Normaltekst className={"banner__orgnr"}>
+              {"Org.nr " + valgtOrganisasjon.OrganizationNumber}{" "}
+            </Normaltekst>
+          )}
+        </div>
       )}
     </div>
   );
