7f665ffe578e2534a4b85c866d322e10e93469ba
hilde.heggstad <hilde.heggstad@bekk.no>
2019-04-01 14:14:54 +0200

    fjernet en /

--- src/server/pamProxy.js
+++ src/server/pamProxy.js
@@ -5,13 +5,15 @@ if (process.env.NAIS_CLUSTER_NAME === "dev-sbs"){
     pamUrl= "https://arbeidsplassen-q.nav.no";
 } else if (process.env.NAIS_CLUSTER_NAME ==="prod-sbs"){
     pamUrl="https://arbeidsplassen.nav.no";
+} else {
+    pamUrl = "http://localhost:8080";
 }
 
 const proxyConfig = {
     changeOrigin: true,
     target: pamUrl,
     pathRewrite: {
-        '^/ditt-nav-arbeidsgiver/pam': '/',
+        '^/ditt-nav-arbeidsgiver/pam': '',
     },
     secure: true,
     xfwd: true
