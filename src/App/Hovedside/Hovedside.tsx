44ab14372b894704320c5234f034681ce2b643ea
hilde.heggstad <hilde.heggstad@bekk.no>
2019-04-24 12:55:10 +0200

    fjernet unødvendig import

--- src/App/Hovedside/Hovedside.tsx
+++ src/App/Hovedside/Hovedside.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useContext, useState } from "react";
+import React, { FunctionComponent, useContext } from "react";
 
 import "./Hovedside.less";
 import ArbeidsgiverTelefon from "./ArbeidsgiverTelefon/ArbeidsgiverTelefon";
