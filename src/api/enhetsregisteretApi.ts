c93605477c4536b39b41508e81c78a221c531768
hilde.heggstad <hilde.heggstad@bekk.no>
2019-04-26 13:30:11 +0200

    lagt til feilhåndtering av kall til enhetsregisteret og satt mocking til ikea

--- src/api/enhetsregisteretApi.ts
+++ src/api/enhetsregisteretApi.ts
@@ -1,23 +1,14 @@
-import {
-  EnhetsregisteretOrg,
-  naeringskode1,
-  naeringskode2,
-  naeringskode3,
-  organisasjonsform,
-  postadresse
-} from "../enhetsregisteretOrg";
+import { defaultOrg, EnhetsregisteretOrg } from "../enhetsregisteretOrg";
 import { enhetsregisteretApiLink } from "../lenker";
 
 export async function hentBedriftsInfo(
   orgnr: string
 ): Promise<EnhetsregisteretOrg> {
   let respons = await fetch(enhetsregisteretApiLink(orgnr));
-  const enhet = await respons.json();
-  const {
-    underTvangsavviklingEllerTvangsopplosning,
-    underAvvikling,
-
-    ...annet
-  } = enhet;
-  return annet;
+  if (respons.ok) {
+    const enhet: EnhetsregisteretOrg = await respons.json();
+    return enhet;
+  }
+  console.log("kunne ikke hente informasjon for orgnr: ", orgnr);
+  return defaultOrg;
 }
