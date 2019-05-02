d522d2bd1cb4875c6c0dd7f4d2f13667beaa72d4
hilde.heggstad <hilde.heggstad@bekk.no>
2019-05-02 15:01:30 +0200

    lagd responsiv løsning for mobilversjon for tjenestebokscontainer

--- src/App/Hovedside/Hovedside.tsx
+++ src/App/Hovedside/Hovedside.tsx
@@ -3,12 +3,14 @@ import React, { FunctionComponent, useContext } from "react";
 import "./Hovedside.less";
 import TjenesteBoksContainer from "./TjenesteBoksContainer/TjenesteBoksContainer";
 import NyttigForDegContainer from "./NyttigForDegContainer/NyttigForDegContainer";
+import AltinnContainer from "./AltinnContainer/AltinnContainer";
 
 const Hovedside: FunctionComponent = () => {
   return (
     <div className="forside">
       <TjenesteBoksContainer />
       <NyttigForDegContainer />
+      <AltinnContainer />
     </div>
   );
 };
