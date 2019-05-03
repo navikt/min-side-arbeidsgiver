5668018bdc0db031611a3d44b4f989c716c6eb4a
bendiksi <benibe88@gmail.com>
2019-05-03 12:31:44 +0200

    la til notifikasjonsring for oppgaver i digisyfo

--- src/App/Hovedside/Syfoboks/Syfoboks.tsx
+++ src/App/Hovedside/Syfoboks/Syfoboks.tsx
@@ -1,4 +1,4 @@
-import React, {FunctionComponent, useEffect} from "react";
+import React, {FunctionComponent, useContext, useEffect, useState} from "react";
 import syfoikon from "./syfoikon.svg";
 import { syfoLink } from "../../../lenker";
 import Undertittel from "nav-frontend-typografi/lib/undertittel";
@@ -6,16 +6,21 @@ import { Normaltekst } from "nav-frontend-typografi";
 import Lenke from "nav-frontend-lenker";
 import Innholdsboks from "../Innholdsboks/Innholdsboks";
 import "./Syfoboks.less";
+import {SyfoTilgangContext, SyfoTilgangProvider} from "../../../SyfoTilgangProvider";
 
 interface Props {
   varseltekst?: string;
+
 }
 
 const Syfoboks: FunctionComponent<Props> = props => {
+    const { syfoOppgaverState } = useContext(SyfoTilgangContext);
+
 
   return (
     <Innholdsboks className={"syfoboks"}>
       <img className={"syfoboks__icon"} src={syfoikon} />
+        <mark >{syfoOppgaverState.length}</mark>
       <div className={"syfoboks__tekst"}>
         <Undertittel className={"syfoboks__header"}>
           Dine sykemeldte
