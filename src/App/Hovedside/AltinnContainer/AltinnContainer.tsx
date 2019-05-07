5cf3dfbea188d2ba3ff15a000c55140113cb6dc6
hilde.heggstad <hilde.heggstad@bekk.no>
2019-05-07 17:02:19 +0200

    lagd komponent for altinnlenker

--- src/App/Hovedside/AltinnContainer/AltinnContainer.tsx
+++ src/App/Hovedside/AltinnContainer/AltinnContainer.tsx
@@ -13,7 +13,7 @@ import {
 import "./AltinnContainer.less";
 import Lenkepanel from "nav-frontend-lenkepanel";
 import { Ingress } from "nav-frontend-typografi";
-import nyfane from "./external-link.svg";
+import nyfane from "./AltinnLenke/external-link.svg";
 
 import {
   inntekstmelding,
@@ -21,6 +21,7 @@ import {
   soknadsskjemaLonnstilskudd,
   soknadTilskuddTilMentor
 } from "../../../lenker";
+import AltinnLenke from "./AltinnLenke/AltinnLenke";
 
 const AltinnContainer: FunctionComponent = () => {
   const [typeAntall, settypeAntall] = useState("");
@@ -52,94 +53,71 @@ const AltinnContainer: FunctionComponent = () => {
 
   return (
     <div className={"altinn-container"}>
-      {tilgangTilAltinnState === 2 && (
-        <Ingress className={"altinn-container__tekst"}>
-          Skjema på Altinn
-        </Ingress>
-      )}
+      <Ingress className={"altinn-container__tekst"}>Skjema på Altinn</Ingress>
       <div className={"altinn-container__bokser"}>
-        {riktigRoll1 && (
-          <Lenkepanel
+        {tilgangTilAltinnForTreSkjemaState === TilgangAltinn.TILGANG && (
+          <AltinnLenke
             className={
               "altinn-container__" + typeAntall + " altinn-container__lenke"
             }
             href={soknadskjemaInkluderingstilskudd()}
-            tittelProps={"element"}
-            border={false}
-            linkCreator={(props: any) => (
-              <a target="_blank" {...props}>
-                {props.children}
-              </a>
-            )}
-          >
-            Søk om inkluderingstilskudd
-            <img
-              className={"altinn-container__ikon"}
-              src={nyfane}
-              alt="ikon for å beskrive at lenken åpnes i en ny fane"
-            />
-          </Lenkepanel>
+            tekst={"Søk om inkluderingstilskudd"}
+          />
         )}
-        {riktigRoll1 && (
-          <Lenkepanel
-            className={"altinn-container__" + typeAntall}
-            href={soknadsskjemaLonnstilskudd()}
-            tittelProps={"element"}
-            border={false}
-            linkCreator={(props: any) => (
-              <a target="_blank" {...props}>
-                {props.children}
-              </a>
-            )}
-          >
-            Søk om lønnstilskudd
-            <img
-              className={"altinn-container__ikon"}
-              src={nyfane}
-              alt="ikon for å beskrive at lenken åpnes i en ny fane"
-            />
-          </Lenkepanel>
-        )}
-        {riktigRoll1 && (
-          <Lenkepanel
-            className={"altinn-container__" + typeAntall}
-            href={soknadTilskuddTilMentor()}
-            tittelProps={"element"}
-            border={false}
-            linkCreator={(props: any) => (
-              <a target="_blank" {...props}>
-                {props.children}
-              </a>
-            )}
-          >
-            Søk om tilskudd til mentor
-            <img
-              className={"altinn-container__ikon"}
-              src={nyfane}
-              alt="ikon for å beskrive at lenken åpnes i en ny fane"
-            />
-          </Lenkepanel>
-        )}
-        {riktigRoll2 && (
-          <Lenkepanel
-            className={"altinn-container__" + typeAntall}
-            href={inntekstmelding}
-            tittelProps={"element"}
-            border={false}
-            linkCreator={(props: any) => (
-              <a target="_blank" {...props}>
-                {props.children}
-              </a>
-            )}
-          >
-            Inntektsmelding til NAV
-            <img
-              className={"altinn-container__ikon"}
-              src={nyfane}
-              alt="ikon for å beskrive at lenken åpnes i en ny fane"
-            />
-          </Lenkepanel>
-        )}
+        <Lenkepanel
+          className={"altinn-container__" + typeAntall}
+          href={soknadsskjemaLonnstilskudd()}
+          tittelProps={"element"}
+          border={false}
+          linkCreator={(props: any) => (
+            <a target="_blank" {...props}>
+              {props.children}
+            </a>
+          )}
+        >
+          Søk om lønnstilskudd
+          <img
+            className={"altinn-container__ikon"}
+            src={nyfane}
+            alt="ikon for å beskrive at lenken åpnes i en ny fane"
+          />
+        </Lenkepanel>
+        <Lenkepanel
+          className={"altinn-container__" + typeAntall}
+          href={soknadTilskuddTilMentor()}
+          tittelProps={"element"}
+          border={false}
+          linkCreator={(props: any) => (
+            <a target="_blank" {...props}>
+              {props.children}
+            </a>
+          )}
+        >
+          Søk om tilskudd til mentor
+          <img
+            className={"altinn-container__ikon"}
+            src={nyfane}
+            alt="ikon for å beskrive at lenken åpnes i en ny fane"
+          />
+        </Lenkepanel>
+        <Lenkepanel
+          className={"altinn-container__" + typeAntall}
+          href={inntekstmelding}
+          tittelProps={"element"}
+          border={false}
+          linkCreator={(props: any) => (
+            <a target="_blank" {...props}>
+              {props.children}
+            </a>
+          )}
+        >
+          Inntektsmelding til NAV
+          <img
+            className={"altinn-container__ikon"}
+            src={nyfane}
+            alt="ikon for å beskrive at lenken åpnes i en ny fane"
+          />
+        </Lenkepanel>
       </div>
     </div>
   );
