e4d7dd352d70346300547b19bd4fa9d4ce0d2232
bendiksi <benibe88@gmail.com>
2019-05-20 12:03:18 +0200

    fjernet arbeidsforhold detaljer fra hovedside

--- src/App/Hovedside/Hovedside.tsx
+++ src/App/Hovedside/Hovedside.tsx
@@ -4,18 +4,15 @@ import "./Hovedside.less";
 import TjenesteBoksContainer from "./TjenesteBoksContainer/TjenesteBoksContainer";
 import NyttigForDegContainer from "./NyttigForDegContainer/NyttigForDegContainer";
 import AltinnContainer from "./AltinnContainer/AltinnContainer";
-import {DetaljertArbeidsforhold} from "@navikt/arbeidsforhold";
-
 
 const Hovedside: FunctionComponent = () => {
-    const arbeidsforholdId = "konvertert_af709505-128e-45dc-a241-7e14180f787d";
+
   return (
     <div className="forside">
       <TjenesteBoksContainer />
       <NyttigForDegContainer />
       <AltinnContainer />
 
-        <DetaljertArbeidsforhold arbeidsforholdId={arbeidsforholdId} />
     </div>
   );
 };
