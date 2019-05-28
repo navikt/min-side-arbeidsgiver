83fe9c97b6ba036d7b587ac21eb9c6524b6b6cc7
bendiksi <benibe88@gmail.com>
2019-05-28 15:34:55 +0200

    Hookifiserte OrganisasjoneliseProvider

--- src/OrganisasjonsListeProvider.tsx
+++ src/OrganisasjonsListeProvider.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import { Organisasjon } from "./organisasjon";
 import { hentOrganisasjoner } from "./api/dnaApi";
 
