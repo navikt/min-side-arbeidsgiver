36d11378909a151905a445908f07b710304647e1
bendiksi <benibe88@gmail.com>
2019-06-03 12:16:43 +0200

    Endret Organisasjonslisteprovider til å bruke hooks

--- src/OrganisasjonsListeProvider.tsx
+++ src/OrganisasjonsListeProvider.tsx
@@ -1,37 +1,35 @@
-import React, {Component} from "react";
+import React, { FunctionComponent, useEffect, useState} from "react";
 import { Organisasjon } from "./organisasjon";
 import { hentOrganisasjoner } from "./api/dnaApi";
 
-export interface Context {
-  organisasjoner: Array<Organisasjon>;
-}
 
-interface State {
-  organisasjoner: Array<Organisasjon>;
-}
+export type Context = {
+  organisasjoner:Array<Organisasjon>
+};
 
 const OrganisasjonsListeContext = React.createContext<Context>({} as Context);
 export { OrganisasjonsListeContext };
 
-export class OrganisasjonsListeProvider extends Component<{}, State> {
-  state: State = {
-    organisasjoner: []
+export const OrganisasjonsListeProvider: FunctionComponent = (props) => {
+
+  const[organisasjoner, setOrganisasjoner] = useState(Array<Organisasjon>());
+
+  useEffect( ()=> {
+    const getOrganisasjoner = async () => {
+      let organisasjoner = await hentOrganisasjoner();
+      setOrganisasjoner(organisasjoner);
+    };
+    getOrganisasjoner();
+  },[]);
+
+  let defaultContext: Context = {
+    organisasjoner
   };
 
-  async componentDidMount() {
-    let organisasjoner = await hentOrganisasjoner();
-    this.setState({ organisasjoner });
-  }
-
-  render() {
-    const context: Context = {
-      ...this.state
-    };
-
     return (
-      <OrganisasjonsListeContext.Provider value={context}>
-        {this.props.children}
+      <OrganisasjonsListeContext.Provider value={defaultContext}>
+        {props.children}
       </OrganisasjonsListeContext.Provider>
     );
-  }
-}
+  };
+
