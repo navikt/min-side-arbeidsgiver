47e4272fa075f8a8026d7fa371fb03a2927aede5
hilde.heggstad <hilde.heggstad@bekk.no>
2019-06-07 17:20:09 +0200

    lagt til ikon i banner

--- src/App/LoggInn/LoggInnBanner/LoggInnBanner.tsx
+++ src/App/LoggInn/LoggInnBanner/LoggInnBanner.tsx
@@ -1,12 +1,16 @@
 import React, { FunctionComponent } from "react";
 import "./LoggInnBanner.less";
 import Sidetittel from "nav-frontend-typografi/lib/sidetittel";
+import ikonhender from "./hender-sjekkliste.svg";
+import dialog from "./dialog.svg";
 
 const LoggInnBanner: FunctionComponent = () => {
   return (
-    <div className={"banner"}>
+    <div className={"logg-inn-banner"}>
       <Sidetittel>NAVs tjenester for arbeidsgivere </Sidetittel>
       <Sidetittel>samlet på et sted </Sidetittel>
+      <img src={ikonhender} className={"logg-inn-banner__hender-ikon"} />
+      <img src={dialog} className={"logg-inn-banner__dialog-ikon"} />
     </div>
   );
 };
