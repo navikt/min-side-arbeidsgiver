a6f5063eef60a32c0e3527a42d698ff73e1543a1
hilde.heggstad <hilde.heggstad@bekk.no>
2019-07-03 17:14:09 +0200

    begynt å endre skalering i nyttig for deg

--- src/App/Hovedside/NyttigForDegContainer/NyttigForDegContainer.tsx
+++ src/App/Hovedside/NyttigForDegContainer/NyttigForDegContainer.tsx
@@ -1,27 +1,42 @@
-import React, { FunctionComponent, useContext } from "react";
+import React, {
+  FunctionComponent,
+  useContext,
+  useState,
+  useEffect
+} from "react";
 import Bedriftsinfoknapp from "./Bedriftsinfoknapp/Bedriftsinfoknapp";
 import "./NyttigForDegContainer.less";
 import KontaktOss from "./KontaktOss/KontaktOss";
 import ArbeidsgiverTelefon from "./ArbeidsgiverTelefon/ArbeidsgiverTelefon";
 import { Ingress } from "nav-frontend-typografi";
 import { OrganisasjonsListeContext } from "../../../OrganisasjonsListeProvider";
+import { TilgangPam } from "../../../OrganisasjonDetaljerProvider";
+import { TilgangSyfo } from "../../../SyfoTilgangProvider";
+import { pamSettBedriftLenke } from "../../../lenker";
 
 const NyttigForDegContainer: FunctionComponent = () => {
   const { organisasjoner } = useContext(OrganisasjonsListeContext);
+  const [visBedriftsinfo, setVisBedriftsinfo] = useState("ikke-synlig");
+
+  useEffect(() => {
+    if (organisasjoner.length > 0) {
+      setVisBedriftsinfo("synlig");
+    }
+  }, [organisasjoner]);
 
   return (
     <div className={"nyttig-for-deg"}>
       <Ingress className={"nyttig-for-deg__tekst"}>Nyttig for deg</Ingress>
-      <div className={"nyttig-for-deg__bokser"}>
+      <div className={"nyttig-for-deg__bokser" + visBedriftsinfo}>
         {organisasjoner.length > 0 && (
-          <div className={"nyttig-for-deg__innholdsboks"}>
+          <div className={"nyttig-for-deg-innholdsboks"}>
             <Bedriftsinfoknapp />
           </div>
         )}
-        <div className={"nyttig-for-deg__innholdsboks"}>
+        <div className={"nyttig-for-deg-innholdsboks"}>
           <KontaktOss />
         </div>
-        <div className={"nyttig-for-deg__innholdsboks"}>
+        <div className={"nyttig-for-deg-innholdsboks"}>
           <ArbeidsgiverTelefon />
         </div>
       </div>
