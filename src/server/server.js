dd9db50aceb2a44c707d6e8bc214d1bac7f3ddbe
bendiksi <benibe88@gmail.com>
2019-07-03 10:12:42 +0200

    la til mocking av veilarbstatus

--- src/server/server.js
+++ src/server/server.js
@@ -46,14 +46,7 @@ const startServer = html => {
     console.log("start server");
     server.use(BASE_PATH, express.static(buildPath,{index: false}));
 
-    server.get(
-        `${BASE_PATH}/internal/isAlive`,
-        (req, res) => res.sendStatus(200)
-    );
-    server.get(
-        `${BASE_PATH}/internal/isReady`,
-        (req, res) => res.sendStatus(200)
-    );
+    setInternalEndpoints();
     server.get(`${BASE_PATH}/*`, (req, res) => {
         res.send(html);
     });
@@ -65,6 +58,17 @@ const startMockServer = html => {
     console.log("start server");
     server.use(BASE_PATH, express.static(buildPath));
 
+    setInternalEndpoints();
+
+    server.get(`${BASE_PATH}/*`, (req, res) => {
+        res.sendFile(path.resolve(buildPath, 'index.html'));
+    });
+    server.listen(port, () => {
+        console.log('Server listening on port', port);
+    });
+};
+
+const setInternalEndpoints = () => {
     server.get(
         `${BASE_PATH}/internal/isAlive`,
         (req, res) => res.sendStatus(200)
@@ -73,13 +77,11 @@ const startMockServer = html => {
         `${BASE_PATH}/internal/isReady`,
         (req, res) => res.sendStatus(200)
     );
-    server.get(`${BASE_PATH}/*`, (req, res) => {
-        res.sendFile(path.resolve(buildPath, 'index.html'));
-    });
-    server.listen(port, () => {
-        console.log('Server listening on port', port);
-    });
 };
+
+
+
+
 if(process.env.REACT_APP_MOCK){
     startMockServer();
 
