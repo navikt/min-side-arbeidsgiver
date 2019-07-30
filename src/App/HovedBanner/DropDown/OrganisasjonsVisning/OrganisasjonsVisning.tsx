4e0a345b21cf2cba4d4955760a63a3d5e777e87e
hilde.heggstad <hilde.heggstad@bekk.no>
2019-07-30 15:01:49 +0200

    fikset så bedriftsikonet blir hvitt onhover

--- src/App/HovedBanner/DropDown/OrganisasjonsVisning/OrganisasjonsVisning.tsx
+++ src/App/HovedBanner/DropDown/OrganisasjonsVisning/OrganisasjonsVisning.tsx
@@ -1,6 +1,7 @@
 import React, { FunctionComponent } from "react";
 import "./OrganisasjonsVisning.less";
 import bedriftsikon from "./bedriftsikon.svg";
+import hvittbedriftsikon from "./hvit-bedrift.svg";
 import { Element } from "nav-frontend-typografi";
 import { Organisasjon } from "../../../../Objekter/organisasjon";
 
@@ -11,9 +12,10 @@ interface Props {
 
 const OrganisasjonsVisning: FunctionComponent<Props> = props => {
   return (
-    <div className={"ovre-button"}>
-      <img src={bedriftsikon} />
-      <div className="ovre-button-tekst">
+    <div className={"organisasjons-visning"}>
+      <img className={"bedrifts-ikon"} src={bedriftsikon} />
+      <img className={"hvitt-ikon"} src={hvittbedriftsikon} />
+      <div className="organisasjons-visning__tekst">
         <Element>{props.hovedOrganisasjon.Name}</Element>
         org. nr. {props.hovedOrganisasjon.OrganizationNumber}
       </div>
