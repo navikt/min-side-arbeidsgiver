9b91da1772a482ff2886e292e272070bdef8bca2
hilde.heggstad <hilde.heggstad@bekk.no>
2019-07-31 13:53:29 +0200

    endret menykomponenter for niva2menyvalg

---
+++ src/App/HovedBanner/VirksomhetsVelgerNiva1/VirksomhetsVelgerNiva2/MenyObjektNiva2/MenyObjektNiva2.tsx
@@ -0,0 +1,30 @@
+import React, { FunctionComponent } from "react";
+
+import { Organisasjon } from "../../../../../Objekter/organisasjon";
+import OrganisasjonsVisning from "../../OrganisasjonsVisning/OrganisasjonsVisning";
+
+const AriaMenuButton = require("react-aria-menubutton");
+
+interface Props {
+  className?: string;
+  UnderEnhet: Organisasjon;
+}
+
+const MenyObjektNiva2: FunctionComponent<Props> = props => {
+  return (
+    <AriaMenuButton.MenuItem
+      key={props.UnderEnhet.OrganizationNumber}
+      value={props.UnderEnhet.OrganizationNumber}
+      text={props.UnderEnhet.Name}
+      tabIndex={0}
+      className="virksomhets-velger-niva-2__meny-objekt"
+    >
+      <OrganisasjonsVisning
+        hovedOrganisasjon={props.UnderEnhet}
+        className={"virksomhets-velger-niva-2__meny-objekt"}
+      />
+    </AriaMenuButton.MenuItem>
+  );
+};
+
+export default MenyObjektNiva2;
