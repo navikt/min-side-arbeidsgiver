bed017db1533fab3caa7c1b7add3fe6e586e664c
hilde.heggstad <hilde.heggstad@bekk.no>
2019-08-05 16:42:09 +0200

    lagd menyvisningskomponent basert på sok

---
+++ src/App/HovedBanner/VirksomhetsVelgerNiva1/Søkefelt/MenyEtterSok/MenyEtterSok.tsx
@@ -0,0 +1,35 @@
+import React, { FunctionComponent, useContext, useState } from "react";
+import { OverenhetOrganisasjon } from "../../../../../Objekter/organisasjon";
+import MenyObjekt from "../../MenyObjekt/MenyObjekt";
+import MenyObjektNiva2 from "../../MenyObjekt/VirksomhetsVelgerNiva2/MenyObjektNiva2/MenyObjektNiva2";
+
+interface Props {
+  className?: string;
+  ListeMedObjektFraSok: OverenhetOrganisasjon[];
+}
+
+const MenyEtterSok: FunctionComponent<Props> = props => {
+  const OrganisasjonsMenyKomponenter = props.ListeMedObjektFraSok.map(function(
+    organisasjon
+  ) {
+    const UnderOrganisasjonsMenyKomponenter = organisasjon.UnderOrganisasjoner.map(
+      function(underenhet) {
+        return <MenyObjektNiva2 UnderEnhet={underenhet} />;
+      }
+    );
+
+    return (
+      <>
+        <MenyObjekt
+          organisasjon={organisasjon}
+          className={"meny-objekt__juridisk-enhet"}
+        />
+        {UnderOrganisasjonsMenyKomponenter}
+      </>
+    );
+  });
+
+  return <>{OrganisasjonsMenyKomponenter}</>;
+};
+
+export default MenyEtterSok;
