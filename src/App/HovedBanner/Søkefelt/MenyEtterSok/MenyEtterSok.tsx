1beac5ddb83741d5eb32068829543e6640b4de99
hilde.heggstad <hilde.heggstad@bekk.no>
2019-08-05 18:43:09 +0200

    fått hiearkiet til å funke men ikke menykomponent

--- src/App/HovedBanner/Søkefelt/MenyEtterSok/MenyEtterSok.tsx
+++ src/App/HovedBanner/Søkefelt/MenyEtterSok/MenyEtterSok.tsx
@@ -1,28 +1,23 @@
-import React, { FunctionComponent, useContext, useState } from "react";
+import React, { FunctionComponent } from "react";
+
+import OrganisasjonsVisning from "../VirksomhetsVelgerNiva1/OrganisasjonsVisning/OrganisasjonsVisning";
+import MenyObjektNiva2 from "../VirksomhetsVelgerNiva1/MenyObjekt/VirksomhetsVelgerNiva2/MenyObjektNiva2/MenyObjektNiva2";
 import {
   Organisasjon,
   OverenhetOrganisasjon
 } from "../../../../Objekter/organisasjon";
-import OrganisasjonsVisning from "../VirksomhetsVelgerNiva1/OrganisasjonsVisning/OrganisasjonsVisning";
-import MenyObjektNiva2 from "../VirksomhetsVelgerNiva1/MenyObjekt/VirksomhetsVelgerNiva2/MenyObjektNiva2/MenyObjektNiva2";
 
-interface Props {
-  className?: string;
+export interface Props {
   ListeMedObjektFraSok: OverenhetOrganisasjon[];
 }
 
 const MenyEtterSok: FunctionComponent<Props> = props => {
-  const OrganisasjonsMenyKomponenter = props.ListeMedObjektFraSok.map(function(
-    juridiskEnhet
+  const menyKomponenter = props.ListeMedObjektFraSok.map(function(
+    juridiskEnhet: OverenhetOrganisasjon
   ) {
     const UnderOrganisasjonsMenyKomponenter = juridiskEnhet.UnderOrganisasjoner.map(
       function(underenhet: Organisasjon) {
-        return (
-          <MenyObjektNiva2
-            organisasjon={underenhet}
-            className={"virksomhets-velger-niva-2__meny-objekt"}
-          />
-        );
+        return <MenyObjektNiva2 organisasjon={underenhet} />;
       }
     );
 
@@ -37,7 +32,7 @@ const MenyEtterSok: FunctionComponent<Props> = props => {
     );
   });
 
-  return <>{OrganisasjonsMenyKomponenter}</>;
+  return <>{menyKomponenter}</>;
 };
 
 export default MenyEtterSok;
