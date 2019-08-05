4126c6dffa101498b506b0000df213bdba3900f6
hilde.heggstad <hilde.heggstad@bekk.no>
2019-08-05 12:39:38 +0200

    laget funkson som returnerer underenheter baser på sok

--- src/App/HovedBanner/VirksomhetsVelgerNiva1/Søkefelt/MenyEtterSok/MenyEtterSok.tsx
+++ src/App/HovedBanner/VirksomhetsVelgerNiva1/Søkefelt/MenyEtterSok/MenyEtterSok.tsx
@@ -8,27 +8,18 @@ const AriaMenuButton = require("react-aria-menubutton");
 interface Props {
   className?: string;
   ListeMedResultatEtterSok: Organisasjon[];
+  UnderEnhet: Organisasjon;
 }
 
 const MenyEtterSok: FunctionComponent<Props> = props => {
-  const OrganisasjonsMenyKomponenter = props.ListeMedResultatEtterSok.UnderOrganisasjoner.map(
-    function(organisasjon: Organisasjon) {
-      return <MenyObjekt UnderEnhet={organisasjon} />;
-    }
-  );
   return (
     <AriaMenuButton.MenuItem
-      key={props.UnderEnhet.OrganizationNumber}
-      value={props.UnderEnhet.OrganizationNumber}
-      text={props.UnderEnhet.Name}
+      key={"hello"}
+      value={"hello"}
+      text={"hello"}
       tabIndex={0}
       className="virksomhets-velger-niva-2__meny-objekt"
-    >
-      <OrganisasjonsVisning
-        hovedOrganisasjon={props.UnderEnhet}
-        className={"virksomhets-velger-niva-2__meny-objekt"}
-      />
-    </AriaMenuButton.MenuItem>
+    />
   );
 };
 
--- src/App/HovedBanner/VirksomhetsVelgerNiva1/Søkefelt/Søkefelt.tsx
+++ src/App/HovedBanner/VirksomhetsVelgerNiva1/Søkefelt/Søkefelt.tsx
@@ -7,9 +7,13 @@ import React, {
 import { Input } from "nav-frontend-skjema";
 import { OrganisasjonsListeContext } from "../../../../OrganisasjonsListeProvider";
 import Innholdsboks from "../../../Hovedside/Innholdsboks/Innholdsboks";
-import {Organisasjon, OverenhetOrganisasjon, tomAltinnOrganisasjon} from "../../../../Objekter/organisasjon";
-import {EnhetsregisteretOrg} from "../../../../Objekter/enhetsregisteretOrg";
-import {hentOverordnetEnhet} from "../../../../api/enhetsregisteretApi";
+import {
+  Organisasjon,
+  OverenhetOrganisasjon,
+  tomAltinnOrganisasjon
+} from "../../../../Objekter/organisasjon";
+import { EnhetsregisteretOrg } from "../../../../Objekter/enhetsregisteretOrg";
+import { hentOverordnetEnhet } from "../../../../api/enhetsregisteretApi";
 const fuzzysort = require("fuzzysort");
 
 interface Props {
@@ -17,39 +21,49 @@ interface Props {
 }
 
 const Sokefelt: FunctionComponent<Props> = props => {
-  const { organisasjonstre, organisasjoner } = useContext(OrganisasjonsListeContext);
+  const { organisasjonstre, organisasjoner } = useContext(
+    OrganisasjonsListeContext
+  );
   const [inputTekst, setInputTekst] = useState("");
-  const [sokeResultatUnderEnheter, setSokeResultatUnderEnheter] = useState(organisasjonstre);
-  const [sokeResultatJuridiskeEnheter, setSokeResultatJuridiskeEnheter] = useState(organisasjonstre);
+  const [sokeResultatUnderEnheter, setSokeResultatUnderEnheter] = useState(
+    organisasjoner
+  );
+  const [
+    sokeResultatJuridiskeEnheter,
+    setSokeResultatJuridiskeEnheter
+  ] = useState(organisasjonstre);
 
   const HentTekstOgSettState = (event: any) => {
     setInputTekst(event.currentTarget.value);
-    setSokeResultatUnderEnheter(fuzzysort.go(inputTekst, organisasjonstre, { key: "Name" }));
+    setSokeResultatUnderEnheter(
+      fuzzysort.go(inputTekst, organisasjonstre, { key: "Name" })
+    );
     console.log(sokeResultatUnderEnheter);
   };
 
-
   const lagSokeResultatListe = (): OverenhetOrganisasjon[] => {
-    let kopiAvOrganisansonsTre: OverenhetOrganisasjon[] = [];
-    let SokeResultat: OverenhetOrganisasjon[];
-    kopiAvOrganisansonsTre.forEach( juridiskEnhet => {
-      let listeMedUnderEnheterFraSokeResultat: Organisasjon[];
+    let sokeResultat: OverenhetOrganisasjon[];
+    sokeResultat = organisasjonstre.map(juridiskEnhet => {
+      let listeMedUnderEnheterFraSokeResultat: Organisasjon[] = [];
       juridiskEnhet.UnderOrganisasjoner.forEach(underenhet => {
-        if (sokeResultatUnderEnheter.find(underenhet)) {
+        if (
+          sokeResultatUnderEnheter.find(underenhet => underenhet === underenhet)
+        ) {
           listeMedUnderEnheterFraSokeResultat.push(underenhet);
-
         }
       });
+      return {
+        overordnetOrg: juridiskEnhet.overordnetOrg,
+        UnderOrganisasjoner: listeMedUnderEnheterFraSokeResultat
+      };
+    });
 
+    return sokeResultat;
+  };
 
+  const sokeresultat = lagSokeResultatListe();
+  console.log(sokeresultat);
 
-
-
-
-    }
-    return SokeResultat;
-
-  }
   return (
     <Innholdsboks>
       <Input
