6554d33efc2ed7105dacd6f0e9478dbaaefc4516
bendiksi <benibe88@gmail.com>
2019-08-29 10:55:06 +0200

    la til tooltip på digisyfoboks

--- src/App/Hovedside/TjenesteBoksContainer/Syfoboks/Syfoboks.tsx
+++ src/App/Hovedside/TjenesteBoksContainer/Syfoboks/Syfoboks.tsx
@@ -5,15 +5,40 @@ import "./Syfoboks.less";
 import TjenesteBoksBanner from "../TjenesteBoksBanner/TjenesteBoksBanner";
 import { syfoLink } from "../../../../lenker";
 import { SyfoTilgangContext } from "../../../../SyfoTilgangProvider";
+import { SyfoOppgave } from "../../../../syfoOppgaver";
 
 interface Props {
   varseltekst?: string;
   className?: string;
 }
 
+const tellTypeOppgaver = (oppgaveArray:SyfoOppgave[], oppgaveType:string):number => {
+  return oppgaveArray.filter(oppgave => {
+    return oppgave.oppgavetype===  oppgaveType}).length;
+};
+
+const leggTilErHvisFlertall = (antall:number, substantiv:string):string=>{
+  if(antall === 1 ){
+    return substantiv;
+  }
+  return substantiv + "er";
+};
+
+
 const Syfoboks: FunctionComponent<Props> = props => {
   const { syfoOppgaverState } = useContext(SyfoTilgangContext);
   const { syfoAnsatteState } = useContext(SyfoTilgangContext);
+
+  const tooltipText:FunctionComponent<Props>=()=>{
+    const antallSykemeldingsvarsler =tellTypeOppgaver(syfoOppgaverState,"Sykemelding");
+    const antallSoknadsVarsler = tellTypeOppgaver(syfoOppgaverState,"Sykepengesøknad");
+
+    return<div>
+      <div>{antallSykemeldingsvarsler} uleste {leggTilErHvisFlertall(antallSykemeldingsvarsler,"sykmelding")}.</div>
+      <div>{antallSoknadsVarsler} {leggTilErHvisFlertall(antallSoknadsVarsler,"sykepengesøknad")} som ikke er lest, eller sendt inn.</div>
+    </div>
+  };
+
   return (
     <div className={"syfoboks " + props.className}>
       <TjenesteBoksBanner
@@ -21,8 +46,8 @@ const Syfoboks: FunctionComponent<Props> = props => {
         imgsource={syfoikon}
         altTekst={""}
         antallVarsler={syfoOppgaverState.length}
+        toolTipext={tooltipText}
       />
-
       <Lenkepanel
         className={"syfoboks__sykemeldte"}
         href={syfoLink()}
