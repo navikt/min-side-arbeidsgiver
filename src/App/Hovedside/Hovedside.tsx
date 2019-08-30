566bf6867700954779ba4df4c8cf22358d652aad
bendiksi <benibe88@gmail.com>
2019-08-30 13:54:40 +0200

    justering av venting på tilgangssjekk

--- src/App/Hovedside/Hovedside.tsx
+++ src/App/Hovedside/Hovedside.tsx
@@ -6,16 +6,15 @@ import NyttigForDegContainer from './NyttigForDegContainer/NyttigForDegContainer
 import AltinnContainer from './AltinnContainer/AltinnContainer';
 import ManglerTilgangBoks from './ManglerTilgangBoks/ManglerTilgangBoks';
 
-import { OrganisasjonsDetaljerContext, TilgangPam } from "../../OrganisasjonDetaljerProvider";
+import { OrganisasjonsDetaljerContext} from "../../OrganisasjonDetaljerProvider";
 import { OrganisasjonsListeContext } from '../../OrganisasjonsListeProvider';
 import { basename } from '../../paths';
 import Lenke from 'nav-frontend-lenker';
 import ikon from './infomation-circle-2.svg';
 import { SkjemaveilederContainer } from './SkjemaveilederContainer/SkjemaveilederContainer';
-import { TilgangSyfo } from "../../SyfoTilgangProvider";
 
 const Hovedside: FunctionComponent = () => {
-    const { harNoenTilganger,tilgangTilPamState,tilgangTilSyfoState } = useContext(OrganisasjonsDetaljerContext);
+    const { harNoenTilganger } = useContext(OrganisasjonsDetaljerContext);
     const { organisasjoner } = useContext(OrganisasjonsListeContext);
     const skalViseManglerTilgangBoks = !(organisasjoner.length > 0 || harNoenTilganger);
 
