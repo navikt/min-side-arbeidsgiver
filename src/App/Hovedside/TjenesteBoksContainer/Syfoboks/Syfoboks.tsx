a0d3c1d73e7dc7ba9a0c3369b3af90679cb8b52e
Bendik Ibenholt <bendik.segrov.ibenholt@nav.no>
2019-08-30 15:36:44 +0200

    Tag 775 tooltip digisyfo varsel (#75)
    * la til tooltip på digisyfoboks
    
    * endre tkst i digisyfo tooltip avhengig av hvor mange varsler det er av hver type
    
    * fiks etter merge
    
    * fiks mocking
    
    * endret id på tooltip
    
    * fjernet en console log linje
    
    * fjernet sårbarhet

--- src/App/Hovedside/TjenesteBoksContainer/Syfoboks/Syfoboks.tsx
+++ src/App/Hovedside/TjenesteBoksContainer/Syfoboks/Syfoboks.tsx
@@ -1,38 +1,76 @@
-import React, { FunctionComponent, useContext } from 'react';
-import syfoikon from './syfoikon.svg';
-import Lenkepanel from 'nav-frontend-lenkepanel';
-import './Syfoboks.less';
-import TjenesteBoksBanner from '../TjenesteBoksBanner/TjenesteBoksBanner';
-import { syfoLink } from '../../../../lenker';
-import { SyfoTilgangContext } from '../../../../SyfoTilgangProvider';
+import React, { FunctionComponent, useContext } from "react";
+import syfoikon from "./syfoikon.svg";
+import Lenkepanel from "nav-frontend-lenkepanel";
+import "./Syfoboks.less";
+import TjenesteBoksBanner from "../TjenesteBoksBanner/TjenesteBoksBanner";
+import { syfoLink } from "../../../../lenker";
+import { SyfoTilgangContext } from "../../../../SyfoTilgangProvider";
+import { SyfoOppgave } from "../../../../Objekter/syfoOppgaver";
 
 interface Props {
     varseltekst?: string;
     className?: string;
 }
 
-const Syfoboks: FunctionComponent<Props> = props => {
-    const { syfoOppgaverState } = useContext(SyfoTilgangContext);
-    const { syfoAnsatteState } = useContext(SyfoTilgangContext);
-    return (
-        <div className={'syfoboks ' + props.className}>
-            <TjenesteBoksBanner
-                tittel={'Sykemeldte'}
-                imgsource={syfoikon}
-                altTekst={''}
-                antallVarsler={syfoOppgaverState.length}
-            />
+const tellTypeOppgaver = (oppgaveArray:SyfoOppgave[], oppgaveType:string):number => {
+  return oppgaveArray.filter(oppgave => {
+    return oppgave.oppgavetype===  oppgaveType}).length;
+};
 
-            <Lenkepanel
-                className={'syfoboks__sykemeldte'}
-                href={syfoLink()}
-                tittelProps={'normaltekst'}
-                linkCreator={(props: any) => <a {...props}>{props.children}</a>}
-            >
-                {syfoAnsatteState} sykemeldte som du har ansvar for å følge opp
-            </Lenkepanel>
-        </div>
-    );
+const LagSykepengesoknadVarselTekst = (antall:number):string=>{
+  if(antall===1){
+    return antall + " sykepenegsøknad som er ulest eller ikke sendt inn"
+  }
+  if(antall > 1){
+    return antall + " sykepengesøknader som er uleste eller ikke sendt inn"
+  }
+  return "";
+};
+
+const LagSykemeldingsVarselTekst = (antall:number):string=>{
+  if(antall===1){
+    return antall + " ulest sykemelding"
+  }
+  if(antall > 1){
+    return antall + " uleste sykemeldinger"
+  }
+  return "";
+};
+
+
+const Syfoboks: FunctionComponent<Props> = props => {
+  const { syfoOppgaverState } = useContext(SyfoTilgangContext);
+  const { syfoAnsatteState } = useContext(SyfoTilgangContext);
+
+  const tooltipText:FunctionComponent<Props>=()=>{
+    const antallSykemeldingsvarsler =tellTypeOppgaver(syfoOppgaverState,"Sykemelding");
+    const antallSoknadsVarsler = tellTypeOppgaver(syfoOppgaverState,"Sykepengesøknad");
+
+    return<div>
+      <div>{LagSykemeldingsVarselTekst(antallSykemeldingsvarsler)}</div>
+      <div>{LagSykepengesoknadVarselTekst(antallSoknadsVarsler)}</div>
+    </div>
+  };
+
+  return (
+    <div className={"syfoboks " + props.className}>
+      <TjenesteBoksBanner
+        tittel={"Sykemeldte"}
+        imgsource={syfoikon}
+        altTekst={""}
+        antallVarsler={syfoOppgaverState.length}
+        toolTipext={tooltipText}
+      />
+      <Lenkepanel
+        className={"syfoboks__sykemeldte"}
+        href={syfoLink()}
+        tittelProps={"normaltekst"}
+        linkCreator={(props: any) => <a {...props}>{props.children}</a>}
+      >
+        {syfoAnsatteState} sykemeldte som du har ansvar for å følge opp
+      </Lenkepanel>
+    </div>
+  );
 };
 
 export default Syfoboks;
