63f97e9b754bc141cde1dd831537038a785b7e2b
hilde.heggstad <hilde.heggstad@bekk.no>
2019-09-09 16:17:55 +0200

    fikset så det telles riktig antall rader fot mobilversjon

--- src/App/InformasjonOmBedriftOgAnsatte/MineAnsatte/TabellMineAnsatte/TabellMineAnsatte.tsx
+++ src/App/InformasjonOmBedriftOgAnsatte/MineAnsatte/TabellMineAnsatte/TabellMineAnsatte.tsx
@@ -11,29 +11,17 @@ const TabellMineAnsatte: FunctionComponent<Props> = props => {
 
     const rader = mineAnsatte.map(arbeidsforhold => (
         <tr className={'tr'} tabIndex={0}>
-            <td className={'td'} tabIndex={0}>
-                Kjell Magne
-            </td>
-            <td className={'td'} tabIndex={0}>
-                {arbeidsforhold.arbeidstaker.offentligIdent}
-            </td>
-            <td className={'td'} tabIndex={0}>
-                {arbeidsforhold.arbeidsavtaler[0].yrke}
-            </td>
-            <td className={'td'} tabIndex={0}>
-                {arbeidsforhold.ansettelsesperiode.periode.fom}
-            </td>
-            <td className={'td'} tabIndex={0}>
-                {arbeidsforhold.ansettelsesperiode.periode.tom}
-            </td>
-            <td className={'td'} tabIndex={0}>
-                varslinger
-            </td>
+            <td className={'td'}>Kjell Magne</td>
+            <td className={'td'}>{arbeidsforhold.arbeidstaker.offentligIdent}</td>
+            <td className={'td'}>{arbeidsforhold.arbeidsavtaler[0].yrke}</td>
+            <td className={'td'}>{arbeidsforhold.ansettelsesperiode.periode.fom}</td>
+            <td className={'td'}>{arbeidsforhold.ansettelsesperiode.periode.tom}</td>
+            <td className={'td'}>varslinger</td>
         </tr>
     ));
 
     return (
-        <table id="arbeidsforholdTable" className={props.className} tabIndex={0}>
+        <table id="arbeidsforholdTable" className={props.className}>
             <thead className="thead" tabIndex={0}>
                 <tr>
                     <th className={'th'} tabIndex={0}>
