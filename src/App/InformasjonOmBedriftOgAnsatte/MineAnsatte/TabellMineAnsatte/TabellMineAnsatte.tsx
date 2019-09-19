b0461331e2039811986f346cdaaef0d9ea7097f9
hilde.heggstad <hilde.heggstad@bekk.no>
2019-09-19 16:31:10 +0200

    klart å skaffe index til navn for å sjekke at det funker å bla

--- src/App/InformasjonOmBedriftOgAnsatte/MineAnsatte/TabellMineAnsatte/TabellMineAnsatte.tsx
+++ src/App/InformasjonOmBedriftOgAnsatte/MineAnsatte/TabellMineAnsatte/TabellMineAnsatte.tsx
@@ -9,28 +9,32 @@ interface Props {
 }
 
 const TabellMineAnsatte: FunctionComponent<Props> = props => {
-    const rader = props.listeMedArbeidsForhold.arbeidsforholdoversikter.map(arbeidsforhold => (
-        <tr>
-            <td className={'td'} tabIndex={0}>
-                {arbeidsforhold.navn}
-            </td>
-            <td className={'td'} tabIndex={0}>
-                {arbeidsforhold.arbeidstaker.offentligIdent}
-            </td>
-            <td className={'td'} tabIndex={0}>
-                {arbeidsforhold.yrke}
-            </td>
-            <td className={'td'} tabIndex={0}>
-                {arbeidsforhold.ansattFom}
-            </td>
-            <td className={'td'} tabIndex={0}>
-                {arbeidsforhold.ansattTom}
-            </td>
-            <td className={'td'} tabIndex={0}>
-                varslinger
-            </td>
-        </tr>
-    ));
+    let index: number = 0;
+    const rader = props.listeMedArbeidsForhold.arbeidsforholdoversikter.map(arbeidsforhold => {
+        index++;
+        return (
+            <tr>
+                <td className={'td'} tabIndex={0}>
+                    {arbeidsforhold.navn + index.toString()}
+                </td>
+                <td className={'td'} tabIndex={0}>
+                    {arbeidsforhold.arbeidstaker.offentligIdent}
+                </td>
+                <td className={'td'} tabIndex={0}>
+                    {arbeidsforhold.yrke}
+                </td>
+                <td className={'td'} tabIndex={0}>
+                    {arbeidsforhold.ansattFom}
+                </td>
+                <td className={'td'} tabIndex={0}>
+                    {arbeidsforhold.ansattTom}
+                </td>
+                <td className={'td'} tabIndex={0}>
+                    varslinger
+                </td>
+            </tr>
+        );
+    });
 
     return (
         <table id="arbeidsforholdTable" className={props.className}>
--- src/mock/AAregisteretMock.tsx
+++ src/mock/AAregisteretMock.tsx
@@ -8,14 +8,30 @@ export const LagRespons = (
     antallArbeidsForhold: number
 ): ObjektFraAAregisteret => {
     let StortObjekt: ObjektFraAAregisteret = liteObjekt;
-    for (let i: number = 1; i < antallArbeidsForhold; i++) {
+    let a: number = 0;
+    for (let i: number = 0; i < antallArbeidsForhold / 4; i++) {
         StortObjekt.arbeidsforholdoversikter.push(liteObjekt.arbeidsforholdoversikter[0]);
-        StortObjekt.arbeidsforholdoversikter[i].navn =
-            liteObjekt.arbeidsforholdoversikter[0] + i.toString();
+        a = a + 1;
+        StortObjekt.arbeidsforholdoversikter[i].navn = 'Guro';
+    }
+    for (let j: number = 25; j < antallArbeidsForhold / 2; j++) {
+        StortObjekt.arbeidsforholdoversikter.push(liteObjekt.arbeidsforholdoversikter[0]);
+        a = a + 1;
+        StortObjekt.arbeidsforholdoversikter[j].navn = 'Mons';
+    }
+    for (let k: number = 50; k < antallArbeidsForhold / (3 / 4); k++) {
+        StortObjekt.arbeidsforholdoversikter.push(liteObjekt.arbeidsforholdoversikter[0]);
+        a = a + 1;
+        StortObjekt.arbeidsforholdoversikter[k].navn = 'Kjartan';
+    }
+    for (let l: number = 75; l < antallArbeidsForhold; l++) {
+        StortObjekt.arbeidsforholdoversikter.push(liteObjekt.arbeidsforholdoversikter[0]);
+        a = a + 1;
+        StortObjekt.arbeidsforholdoversikter[l].navn = 'Kjartan';
     }
     return StortObjekt;
 };
 
-export const TestObjekt: ObjektFraAAregisteret = LagRespons(mockObjekt, 1000);
+export const TestObjekt: ObjektFraAAregisteret = LagRespons(mockObjekt, 100);
 
 fetchMock.get('https://www.facebook.com/', TestObjekt);
