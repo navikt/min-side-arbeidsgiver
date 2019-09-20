0830e743117fbacf4fc748b253c953f40cefcebb
hilde.heggstad <hilde.heggstad@bekk.no>
2019-09-20 13:01:21 +0200

    lagd et spesialtilfelle

--- src/App/InformasjonOmBedriftOgAnsatte/MineAnsatte/BlaIAnsatte/VisningAvPageTurner.tsx
+++ src/App/InformasjonOmBedriftOgAnsatte/MineAnsatte/BlaIAnsatte/VisningAvPageTurner.tsx
@@ -1,6 +1,6 @@
 import React, { FunctionComponent, useState, useContext, useEffect } from 'react';
 import './BlaIAnsatte.less';
-import { enkelArbeidsforhold, ObjektFraAAregisteret } from '../../../../Objekter/Ansatte';
+import './VisningAvPageTurner.less';
 import GraSirkelMedNr from './GraSirkelMedNr/GraSirkelMedNr';
 import { OrganisasjonsDetaljerContext } from '../../../../OrganisasjonDetaljerProvider';
 
@@ -8,17 +8,13 @@ import ListeMedAnsatteForMobil from '../ListeMineAnsatteForMobil/ListeMineAnsatt
 import TabellMineAnsatte from '../TabellMineAnsatte/TabellMineAnsatte';
 interface Props {
     className?: string;
-    listeMedArbeidsForhold: ObjektFraAAregisteret;
-    endreSideVisning: (indeks: number) => void;
 }
 
 const VisningAvSideBytter: FunctionComponent<Props> = props => {
-    const [naVarendeIndex, setnaVarendeIndex] = useState(1);
     const { mineAnsatte } = useContext(OrganisasjonsDetaljerContext);
     const [listenSomSkalVises, setListenSomSkalVises] = useState([]);
 
     const setIndeksOgGenererListe = (indeks: number) => {
-        setnaVarendeIndex(indeks);
         const forsteElement: number = 25 * indeks - 24;
         const vise: any = mineAnsatte.arbeidsforholdoversikter.slice(
             forsteElement - 1,
@@ -29,17 +25,14 @@ const VisningAvSideBytter: FunctionComponent<Props> = props => {
 
     return (
         <>
-            <div className={'sidebytter'}>
-                <button className={'sidebytter__valg'} onClick={() => setIndeksOgGenererListe(1)}>
+            <div className={'tre-case'}>
+                <button className={'tre-case__valg'} onClick={() => setIndeksOgGenererListe(1)}>
                     <GraSirkelMedNr sidetall={1} />
                 </button>
-                <button
-                    className="sidebytter__valg erValgt"
-                    onClick={() => setIndeksOgGenererListe(2)}
-                >
+                <button className="tre-case__valg" onClick={() => setIndeksOgGenererListe(2)}>
                     <GraSirkelMedNr sidetall={2} />
                 </button>
-                <button className={'sidebytter__valg'} onClick={() => setIndeksOgGenererListe(3)}>
+                <button className={'tre-case__valg'} onClick={() => setIndeksOgGenererListe(3)}>
                     <GraSirkelMedNr sidetall={3} />
                 </button>
             </div>
--- src/App/InformasjonOmBedriftOgAnsatte/MineAnsatte/MineAnsatte.tsx
+++ src/App/InformasjonOmBedriftOgAnsatte/MineAnsatte/MineAnsatte.tsx
@@ -3,6 +3,7 @@ import './MineAnsatte.less';
 import { Undertittel } from 'nav-frontend-typografi';
 import { OrganisasjonsDetaljerContext } from '../../../OrganisasjonDetaljerProvider';
 import SideBytter from './BlaIAnsatte/BlaIAnsatte';
+import VisningAvSideBytter from './BlaIAnsatte/VisningAvPageTurner';
 
 interface Props {
     className?: string;
@@ -15,7 +16,7 @@ const MineAnsatte: FunctionComponent<Props> = props => {
             <Undertittel className={'mine-ansatte__systemtittel'} tabIndex={0}>
                 Opplysninger fra Aa-registeret
             </Undertittel>
-            <SideBytter listeMedArbeidsForhold={mineAnsatte} className={'sidebytter'} />
+            <VisningAvSideBytter />
             <div tabIndex={0} className={'mine-ansatte__antall'}>
                 {mineAnsatte.arbeidsforholdoversikter.length} arbeidsforhold
             </div>
