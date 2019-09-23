3f3a00f9e9f5402e0b3bdd6ebf92586e7a403a75
hilde.heggstad <hilde.heggstad@bekk.no>
2019-09-23 13:31:22 +0200

    fjernet arbeidstreningikon og endret overskrift til siden

--- src/App/LoggInn/Innloggingstjenester/Innloggingstjenester.tsx
+++ src/App/LoggInn/Innloggingstjenester/Innloggingstjenester.tsx
@@ -2,7 +2,7 @@ import React, { FunctionComponent } from 'react';
 import './Innloggingstjenester.less';
 import medisin from './medicine-capsule-1.svg';
 import sok from './search.svg';
-import verktoy from './toolbox.svg';
+import soylediagram from './soylediagram.svg';
 import skjema from './skjema.svg';
 import { Element } from 'nav-frontend-typografi';
 
@@ -18,16 +18,16 @@ export const Innloggingstjenester: FunctionComponent = () => {
                 <Element>Dine sykmeldte </Element>
             </div>
             <div className={'innloggingsside-tjenester__tjeneste'}>
-                <img src={sok} alt={''} className={'innloggingsside-tjenester__tjeneste-ikon'} />
-                <Element>Rekruttere </Element>
-            </div>
-            <div className={'innloggingsside-tjenester__tjeneste'}>
                 <img
                     alt={''}
-                    src={verktoy}
+                    src={soylediagram}
                     className={'innloggingsside-tjenester__tjeneste-ikon'}
                 />
-                <Element>Arbeidstrening </Element>
+                <Element>Sykefraværsstatistikk </Element>
+            </div>
+            <div className={'innloggingsside-tjenester__tjeneste'}>
+                <img src={sok} alt={''} className={'innloggingsside-tjenester__tjeneste-ikon'} />
+                <Element>Rekruttere </Element>
             </div>
             <div className={'innloggingsside-tjenester__tjeneste'}>
                 <img alt={''} src={skjema} className={'innloggingsside-tjenester__tjeneste-ikon'} />
