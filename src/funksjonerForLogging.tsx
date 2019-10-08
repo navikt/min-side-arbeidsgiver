50017fdf1c446f4813dbc09084a64bdddc28e0c0
hilde.heggstad <hilde.heggstad@bekk.no>
2019-10-08 16:01:19 +0200

    håndtert hvis organisasjonen ikke finnes i enhetsregisteret i loggingen

--- src/funksjonerForLogging.tsx
+++ src/funksjonerForLogging.tsx
@@ -8,18 +8,29 @@ export const hentInfoOgLoggInformasjon = async (organisasjon?: Organisasjon) => 
         const infoFraEreg: OrganisasjonFraEnhetsregisteret = await hentUnderenhet(
             organisasjon.OrganizationNumber
         );
-        logInfo(
-            'besok fra organisasjon: ' + organisasjon.Name,
-            organisasjon.OrganizationNumber + ' ' + infoFraEreg.forretningsadresse.kommune
-        );
-        logInfo('antall ansatte: ' + infoFraEreg.antallAnsatte);
-        const parent: OrganisasjonFraEnhetsregisteret = await hentOverordnetEnhet(
-            organisasjon.ParentOrganizationNumber
-        );
-        logInfo('antall ansatte: ' + infoFraEreg.antallAnsatte);
-        logInfo(
-            'juridisk enhet: ' + parent.navn,
-            parent.organisasjonsnummer + 'antall ansatte i juridisk enhet' + parent.antallAnsatte
-        );
+        logInfo('besok fra organisasjon: ' + organisasjon.Name, organisasjon.OrganizationNumber);
+
+        if (infoFraEreg.organisasjonsnummer !== '') {
+            logInfo(
+                'antall ansatte: ' +
+                    infoFraEreg.antallAnsatte +
+                    'kommune: ' +
+                    infoFraEreg.forretningsadresse.kommune
+            );
+            const parent: OrganisasjonFraEnhetsregisteret = await hentOverordnetEnhet(
+                organisasjon.ParentOrganizationNumber
+            );
+            logInfo('antall ansatte: ' + infoFraEreg.antallAnsatte);
+            logInfo(
+                'juridisk enhet: ' + parent.navn,
+                parent.organisasjonsnummer +
+                    'antall ansatte i juridisk enhet' +
+                    parent.antallAnsatte
+            );
+        }
+
+        if (infoFraEreg.organisasjonsnummer === '') {
+            logInfo('Klarte ikke hente underenhet fra Ereg');
+        }
     }
 };
