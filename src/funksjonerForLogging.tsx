b182984ff3da0969764fc62070c5ccdfb9623776
hilde.heggstad <hilde.heggstad@bekk.no>
2019-10-08 15:42:27 +0200

    endret mocking litt

--- src/funksjonerForLogging.tsx
+++ src/funksjonerForLogging.tsx
@@ -3,21 +3,23 @@ import { Organisasjon } from './Objekter/Organisasjoner/OrganisasjonerFraAltinn'
 import { logInfo } from './utils/metricsUtils';
 import { hentOverordnetEnhet, hentUnderenhet } from './api/enhetsregisteretApi';
 
-export const hentInfoOgLoggInformasjon = async (organisasjon: Organisasjon) => {
-    const infoFraEreg: OrganisasjonFraEnhetsregisteret = await hentUnderenhet(
-        organisasjon.OrganizationNumber
-    );
-    logInfo(
-        'besok fra organisasjon: ' + organisasjon.Name,
-        organisasjon.OrganizationNumber + ' ' + infoFraEreg.forretningsadresse.kommune
-    );
-    logInfo('antall ansatte: ' + infoFraEreg.antallAnsatte);
-    const parent: OrganisasjonFraEnhetsregisteret = await hentOverordnetEnhet(
-        organisasjon.ParentOrganizationNumber
-    );
-    logInfo('antall ansatte: ' + infoFraEreg.antallAnsatte);
-    logInfo(
-        'juridisk enhet: ' + parent.navn,
-        parent.organisasjonsnummer + 'antall ansatte i juridisk enhet' + parent.antallAnsatte
-    );
+export const hentInfoOgLoggInformasjon = async (organisasjon?: Organisasjon) => {
+    if (organisasjon) {
+        const infoFraEreg: OrganisasjonFraEnhetsregisteret = await hentUnderenhet(
+            organisasjon.OrganizationNumber
+        );
+        logInfo(
+            'besok fra organisasjon: ' + organisasjon.Name,
+            organisasjon.OrganizationNumber + ' ' + infoFraEreg.forretningsadresse.kommune
+        );
+        logInfo('antall ansatte: ' + infoFraEreg.antallAnsatte);
+        const parent: OrganisasjonFraEnhetsregisteret = await hentOverordnetEnhet(
+            organisasjon.ParentOrganizationNumber
+        );
+        logInfo('antall ansatte: ' + infoFraEreg.antallAnsatte);
+        logInfo(
+            'juridisk enhet: ' + parent.navn,
+            parent.organisasjonsnummer + 'antall ansatte i juridisk enhet' + parent.antallAnsatte
+        );
+    }
 };
