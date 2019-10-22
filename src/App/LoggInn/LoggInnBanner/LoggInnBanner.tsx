9ff3c165f40c853e05f2e37b75c15cadf0f34ba4
hilde.heggstad <hilde.heggstad@bekk.no>
2019-10-22 13:57:23 +0200

    flekset banneret sådet er riktig

--- src/App/LoggInn/LoggInnBanner/LoggInnBanner.tsx
+++ src/App/LoggInn/LoggInnBanner/LoggInnBanner.tsx
@@ -1,17 +1,26 @@
 import React, { FunctionComponent } from 'react';
 import './LoggInnBanner.less';
 import { Sidetittel, Ingress } from 'nav-frontend-typografi';
+import medhjelm from './med-hjelm.svg';
+import telefon from './telefon-person.svg';
 
 const LoggInnBanner: FunctionComponent = () => {
     return (
         <div className={'logg-inn-banner'}>
-            <div className={'logg-inn-banner__tittel-og-tekst'}>
-                <Sidetittel className={'logg-inn-banner__tittel'}>
-                    Min side - arbeidsgiver{' '}
-                </Sidetittel>
-                <Ingress className={'logg-inn-banner__ingress'}>
-                    Innloggede tjenester for arbeidsgivere{' '}
-                </Ingress>
+            <div className={'logg-inn-banner__container'}>
+                <div className={'logg-inn-banner__tittel-og-tekst'}>
+                    <Sidetittel className={'logg-inn-banner__tittel'}>
+                        Min side - arbeidsgiver{' '}
+                    </Sidetittel>
+                    <Ingress className={'logg-inn-banner__ingress'}>
+                        Innloggede tjenester for arbeidsgivere{' '}
+                    </Ingress>
+                </div>
+                <div className={'logg-inn-banner__bilder'}>
+                    <img src={medhjelm} alt={'Bilde av koffert for å illustrere arbeidsgivere'} />
+                    <img src={telefon} alt={'Bilde av koffert for å illustrere arbeidsgivere'} />
+                    <img src={telefon} alt={'Bilde av koffert for å illustrere arbeidsgivere'} />
+                </div>
             </div>
         </div>
     );
