466583a52844ecc67bdb70ce3fca1be4d6c79987
Ibenholt <bendik.segrov.ibenholt@nav.no>
2019-11-20 14:55:50 +0100

    legg på proxy for digisyfo

---
+++ src/server/digisyfoProxy.js
@@ -0,0 +1,24 @@
+import environment from "../utils/environment";
+
+const proxy = require('http-proxy-middleware');
+
+const sykemeldteTarget = () => {
+    if (process.env.NAIS_CLUSTER_NAME === "prod-sbs") {
+        return "https://tjenester.nav.no";
+    } else {
+        return "https://tjenester-q1.nav.no";
+    }
+};
+
+const digisyfoSykemeldteProxyConfig = {
+    changeOrigin: true,
+    target: sykemeldteTarget(),
+    pathRewrite: {
+        '^/min-side-arbeidsgiver': '',
+    },
+    secure: true,
+    xfwd: true
+};
+
+
+module.exports = proxy(digisyfoSykemeldteProxyConfig);
