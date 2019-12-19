0143412d50d3327959e604e4bcf01587d8ac43b9
Bendik Ibenholt <bendik.segrov.ibenholt@nav.no>
2019-12-19 12:37:00 +0100

    la til visning av driftsmelding i tidsinterval (#109)
    * la til visning av driftsmelding i tidsinterval
    
    * formatering
    
    * fjernet no logging

---
+++ src/App/LoggInn/VarselOmNedetid/VarselOmNedetid.tsx
@@ -0,0 +1,31 @@
+import React, { FunctionComponent } from 'react';
+import AlertStripe from 'nav-frontend-alertstriper';
+
+type VarselOmNedetidProps =
+    | {
+          visVarselOmNedeTid: true;
+          visFraDato: Date;
+          visTilDato: Date;
+      }
+    | {
+          visVarselOmNedeTid: false;
+      };
+
+export const VarselOmNedetid: FunctionComponent<VarselOmNedetidProps> = props => {
+    const dagensDato: Date = new Date();
+    if (
+        !props.visVarselOmNedeTid ||
+        dagensDato > props.visTilDato || dagensDato < props.visFraDato
+    ) {
+        return null;
+    }
+    return (
+        <div>
+            <AlertStripe type={'advarsel'}>
+                {' '}
+                Altinn vil være utilgjengelig 01.01.2020 i tidsrommet 17:00 til ca. 23:00. Dette vil
+                medføre at en del tjenester på Min Side - Arbeidsgiver ikke vil fungere som normalt.
+            </AlertStripe>
+        </div>
+    );
+};
