9d2261f1b09c3a4b76a04c6d5569288bd70ddaae
Hilde Steinbru Heggstad <hilde.steinbru.heggstad@nav.no>
2020-02-12 13:22:36 +0100

    hanterer bruker ikke tilgang til bedrifter

--- src/App/Hovedside/TjenesteBoksContainer/TjenesteBoksContainer.tsx
+++ src/App/Hovedside/TjenesteBoksContainer/TjenesteBoksContainer.tsx
@@ -93,7 +93,7 @@ const TjenesteBoksContainer: FunctionComponent = () => {
     }, [
         organisasjonslisteFerdigLastet,
         organisasjonerMedIAFerdigLastet,
-        tilgangsArray
+        tilgangsArray, organisasjoner
     ]);
 
     return (
