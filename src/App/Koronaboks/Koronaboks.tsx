e96f51d7e7f1e24c9883aacc118c7ad8646d3f65
hilde.heggstad <hilde.heggstad@bekk.no>
2020-03-25 12:36:09 +0100

    fatt inn riktig ikon

--- src/App/Koronaboks/Koronaboks.tsx
+++ src/App/Koronaboks/Koronaboks.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Koronaboks.less';
 import Innholdsboks from '../Hovedside/Innholdsboks/Innholdsboks';
-import Undertittel from 'nav-frontend-typografi/lib/undertittel';
+import Element from 'nav-frontend-typografi/lib/element';
 import Lenke from 'nav-frontend-lenker';
 import HoyreChevron from 'nav-frontend-chevron/lib/hoyre-chevron';
 import Normaltekst from 'nav-frontend-typografi/lib/normaltekst';
-import syfoikon from './syfoikon.svg';
+import sirkel from './report-problem-circle.svg';
 import { lenkeTilPermitteringOgMasseoppsigelsesSkjema } from '../../lenker';
+
 
 
 
@@ -14,8 +15,8 @@ export const Koronaboks = () => {
     return (
         <div className={'koronaboks'}>
         <Innholdsboks classname={'koronaboks__innhold'} >
-            <img className={'koronaboks__ikon'} alt='' src={syfoikon}/>
-            <Undertittel> Koronavirus - permitteringer, oppsigelser eller innskrenkning i arbeidstid</Undertittel>
+            <img className={'koronaboks__ikon'} alt='' src={sirkel}/>
+            <Element> Koronavirus - permitteringer, oppsigelser eller innskrenkning i arbeidstid</Element>
             <Normaltekst className={'koronaboks__tekst'}>Arbeidsgiver har meldeplikt til NAV ved masseoppsigelser, permitteringer uten lønn og innskrenknigner i arbeidstiden, dersom dette rammer mer enn 10 arbeidtakere.</Normaltekst>
             <Lenke href={lenkeTilPermitteringOgMasseoppsigelsesSkjema()}>Varsle NAV gå til skjema <HoyreChevron/></Lenke>
                 <Lenke href={'https://arbeidsgiver.nav.no/permittering/'}>Informasjon til arbeidsgivere <HoyreChevron/></Lenke>
