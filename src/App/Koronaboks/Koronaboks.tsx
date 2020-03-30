fc7e2553d788a167c798a13d8d08d8bd5de02377
Ibenholt <bendik.segrov.ibenholt@nav.no>
2020-03-30 08:54:58 +0100

    la til riktig lenke for refusjons

--- src/App/Koronaboks/Koronaboks.tsx
+++ src/App/Koronaboks/Koronaboks.tsx
@@ -1,24 +1,26 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import './Koronaboks.less';
 import Innholdsboks from '../Hovedside/Innholdsboks/Innholdsboks';
 import Element from 'nav-frontend-typografi/lib/element';
 import Lenke from 'nav-frontend-lenker';
 import HoyreChevron from 'nav-frontend-chevron/lib/hoyre-chevron';
 import sirkel from './report-problem-circle.svg';
-import { lenkeTilPermitteringOgMasseoppsigelsesSkjema } from '../../lenker';
+import {LenkeTilKoronaRefusjon, lenkeTilPermitteringOgMasseoppsigelsesSkjema} from '../../lenker';
 import {Undertittel} from "nav-frontend-typografi";
+import {OrganisasjonsDetaljerContext} from "../../OrganisasjonDetaljerProvider";
 
 export const Koronaboks = () => {
+    const { tilgangsArray } = useContext(OrganisasjonsDetaljerContext);
     return (
         <div className={'koronaboks'}>
         <Innholdsboks classname={'koronaboks__innhold'} >
             <img className={'koronaboks__ikon'} alt='' src={sirkel}/>
-            <Undertittel className="tjeneste-boks-banner__tittel">Koronarelaterte tjenester</Undertittel>
-            <Element> Permittering </Element>
+            <Undertittel >Koronarelaterte tjenester</Undertittel>
+            <Element className={'koronaboks__tekst'}> Permittering </Element>
             <Lenke className={'koronaboks__lenke'} href={lenkeTilPermitteringOgMasseoppsigelsesSkjema()}>Varsle NAV om permitteringer, masseoppsigelser, permitteringer eller innskrenknigner i arbeidstiden <HoyreChevron/></Lenke>
             <Lenke className={'koronaboks__lenke'} href={"https://www.nav.no/person/kontakt-oss/chat/arbeidsgiver"}>Chat med NAV om permittering <HoyreChevron/></Lenke>
-            <Element> Refusjon </Element>
-            <Lenke className={'koronaboks__lenke'} href={"https://www.nav.no/person/kontakt-oss/chat/arbeidsgiver"}>Søk om refusjon av sykepenger relatert til koronavirus <HoyreChevron/></Lenke>
+            <Element className={'koronaboks__tekst'}> Refusjon </Element>
+            <Lenke className={'koronaboks__lenke'} href={LenkeTilKoronaRefusjon()}>Søk om refusjon av sykepenger relatert til koronavirus <HoyreChevron/></Lenke>
         </Innholdsboks>
         </div>
     );
