722e7ad11bcb0ecc52d95eb5927fd21236632577
Terese Haug <terese.haug@nav.no>
2020-05-13 11:10:05 +0200

    Aktiverer Sentry igjen. Oppgraderer bedriftsmeny til nyeste versjon

--- src/index.tsx
+++ src/index.tsx
@@ -3,21 +3,21 @@ import 'regenerator-runtime/runtime';
 import 'unorm/lib/unorm';
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import { init as Sentry } from '@sentry/browser';
 import 'whatwg-fetch';
+import { init as Sentry } from '@sentry/browser';
 import App from './App/App';
 import './index.less';
 
-/* Sentry({
+Sentry({
     dsn: 'https://57108359840e4a28b979e36baf5e5c6c@sentry.gc.nav.no/27',
     release: process.env.GIT_COMMIT_HASH || 'unknown',
     environment: window.location.hostname,
-}); */
+});
 
 if (process.env.REACT_APP_MOCK) {
     console.log('========================================');
     console.log('=============== MED MOCK ===============');
-    console.log('===DETTE SKAL DU IKKE SE I PRODUKSJON===');
+    console.log('== DETTE SKAL DU IKKE SE I PRODUKSJON ==');
     console.log('========================================');
     require('./mock/pamMock');
     require('./mock/syfoMock');
@@ -29,7 +29,3 @@ if (process.env.REACT_APP_MOCK) {
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
