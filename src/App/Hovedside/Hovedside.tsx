f37931ad37cec4fd2859f9dd64f0964c16b25d01
Terese Haug <terese.haug@nav.no>
2020-05-25 12:13:02 +0200

    Redesign på be-om-tilgang/tittel. Viser koronaboks kun for de som har tilgang

--- src/App/Hovedside/Hovedside.tsx
+++ src/App/Hovedside/Hovedside.tsx
@@ -1,8 +1,6 @@
 import React, { FunctionComponent, useContext } from 'react';
-import Lenke from 'nav-frontend-lenker';
 import { OrganisasjonsListeContext } from '../../OrganisasjonsListeProvider';
 import { SyfoTilgangContext } from '../../SyfoTilgangProvider';
-import { basename } from '../../paths';
 import { Tilgang } from '../LoginBoundary';
 import TjenesteBoksContainer from './TjenesteBoksContainer/TjenesteBoksContainer';
 import NyttigForDegContainer from './NyttigForDegContainer/NyttigForDegContainer';
@@ -11,9 +9,8 @@ import { ManglerTilgangContainer } from './ManglerTilgangContainer/ManglerTilgan
 import { FeilmeldingContainer } from './FeilmeldingContainer/FeilmeldingContainer';
 import { SkjemaveilederContainer } from './SkjemaveilederContainer/SkjemaveilederContainer';
 import IkkeTilgangTilDisseTjenestene from './IkkeTilgangTilDisseTjenestene/IkkeTilgangTilDisseTjenestene';
-import ikon from './infomation-circle-2.svg';
-import './Hovedside.less';
 import { Koronaboks } from '../Koronaboks/Koronaboks';
+import './Hovedside.less';
 
 const Hovedside: FunctionComponent = () => {
     const { organisasjoner, visFeilmelding } = useContext(OrganisasjonsListeContext);
@@ -23,35 +20,21 @@ const Hovedside: FunctionComponent = () => {
         organisasjoner.length > 0 || tilgangTilSyfoState === Tilgang.TILGANG
     );
 
-    /*const loggPermitteringsinfo = (lenkebeskrivelse: string) => {
-        loggTjenesteTrykketPa(lenkebeskrivelse);
-    };*/
-
     return (
         <div className="hovedside">
             <FeilmeldingContainer
                 visFeilmelding={visFeilmelding}
                 visSyfoFeilmelding={visSyfoFeilmelding}
             />
-            <Koronaboks/>
             {skalViseManglerTilgangBoks && <ManglerTilgangContainer />}
             {!skalViseManglerTilgangBoks && (
                 <>
+                    <Koronaboks/>
                     <TjenesteBoksContainer />
                     <NyttigForDegContainer />
                     <AltinnContainer />
                     <SkjemaveilederContainer />
                     <IkkeTilgangTilDisseTjenestene />
-                    <div className="hovedside__informasjonstekst">
-                        <img className="hovedside__ikon" src={ikon} alt="informasjonsikon" />
-                        Forventet du å se flere tjenester?
-                        <Lenke
-                            className="hovedside__lenke"
-                            href={basename + '/informasjon-om-tilgangsstyring'}
-                        >
-                            Les mer om hvordan du får tilgang
-                        </Lenke>{' '}
-                    </div>
                 </>
             )}
         </div>
