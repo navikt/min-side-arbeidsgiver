c178bd0982cd5ace6fdd4c830c37960d567c619a
Hilde Steinbru Heggstad <47525174+hildeheggstad@users.noreply.github.com>
2020-09-02 11:15:02 +0200

    Fjern veilarbstepup (#234)
    * fjernet essotoken og veilarbstepup
    
    * fiks altinn handter feil
    
    Co-authored-by: Ibenholt <bendik.segrov.ibenholt@nav.no>

--- src/mock/veilarbMock.ts
+++
@@ -1,9 +0,0 @@
-import fetchMock from 'fetch-mock';
-
-fetchMock.get('/min-side-arbeidsgiver/veilarbstepup/status', {
-    erInnlogget: false,
-    harGyldigOidcToken: false,
-    brukerId: null,
-    niva: null,
-    nivaOidc: null,
-});
