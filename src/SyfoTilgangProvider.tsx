791cad4d1b56bff677d436463b4f5877859c3595
Bendik Ibenholt <bendik.segrov.ibenholt@nav.no>
2020-09-08 15:56:02 +0300

    fjern kall for å hente syfooppgaver og syfonærmesteansatte (#258)
    * fjern kall for å hente syfooppgaver og syfonærmesteansatte
    
    * fjern syfomock og syfoapi og syfolenker

--- src/SyfoTilgangProvider.tsx
+++ src/SyfoTilgangProvider.tsx
@@ -1,15 +1,10 @@
 import React, { FunctionComponent, useEffect, useState } from 'react';
 import { hentSyfoTilgang } from './api/dnaApi';
-import { hentNarmesteAnsate, hentSyfoOppgaver } from './api/digisyfoApi';
-import { SyfoOppgave } from './Objekter/syfoOppgaver';
 import { Tilgang } from './App/LoginBoundary';
 
 export interface Context {
     tilgangTilSyfoState: Tilgang;
-    syfoOppgaverState: Array<SyfoOppgave>;
-    syfoAnsatteState: number;
     visSyfoFeilmelding: boolean;
-    visSyfoOppgaveFeilmelding: boolean;
 }
 
 const SyfoTilgangContext = React.createContext<Context>({} as Context);
@@ -17,10 +12,7 @@ export { SyfoTilgangContext };
 
 export const SyfoTilgangProvider: FunctionComponent = props => {
     const [tilgangTilSyfoState, setTilgangTilSyfoState] = useState(Tilgang.LASTER);
-    const [syfoOppgaverState, setSyfoOppgaverState] = useState(Array<SyfoOppgave>());
-    const [syfoAnsatteState, setSyfoAnsatteState] = useState(0);
     const [visSyfoFeilmelding, setVisSyfoFeilmelding] = useState(false);
-    const [visSyfoOppgaveFeilmelding, setVisSyfoOppgaveFeilmelding] = useState(false);
     useEffect(() => {
         setTilgangTilSyfoState(Tilgang.LASTER);
         let tilgangSyfoRespons = false;
@@ -34,13 +26,6 @@ export const SyfoTilgangProvider: FunctionComponent = props => {
             }
             if (tilgangSyfoRespons) {
                 setTilgangTilSyfoState(Tilgang.TILGANG);
-                try {
-                    setSyfoOppgaverState(await hentSyfoOppgaver());
-                } catch (e) {
-                    setVisSyfoOppgaveFeilmelding(true);
-                }
-                const syfoAnsatteArray = await hentNarmesteAnsate();
-                setSyfoAnsatteState(syfoAnsatteArray.length);
             } else {
                 setTilgangTilSyfoState(Tilgang.IKKE_TILGANG);
             }
@@ -50,10 +35,7 @@ export const SyfoTilgangProvider: FunctionComponent = props => {
 
     let defaultContext: Context = {
         tilgangTilSyfoState,
-        syfoOppgaverState,
-        syfoAnsatteState,
         visSyfoFeilmelding,
-        visSyfoOppgaveFeilmelding,
     };
 
     return (
--- src/mock/syfoMock.ts
+++ src/mock/syfoMock.ts
@@ -1,5 +1,5 @@
 import fetchMock from 'fetch-mock';
-import { digiSyfoNarmesteLederLink, digisyfoSykemeldteLenke } from '../lenker';
+import { digiSyfoNarmesteLederLink } from '../lenker';
 
 const delay = new Promise(res => setTimeout(res, 1500));
 
@@ -10,60 +10,6 @@ fetchMock
             return {
                 tilgang: true,
             };
-        })
-    )
-    .spy();
-
-fetchMock
-    .get(digisyfoSykemeldteLenke(), [
-        {
-            fnr: '',
-            aktoerId: '1397174971178',
-            orgnummer: '910532308',
-            koblingId: 30968,
-            navn: null,
-        },
-        {
-            fnr: '',
-            aktoerId: '1563540214911',
-            orgnummer: '910532251',
-            koblingId: 30969,
-            navn: null,
-        },
-        {
-            fnr: '',
-            aktoerId: '1113726300051',
-            orgnummer: '910532308',
-            koblingId: 30801,
-            navn: null,
-        },
-    ])
-    .spy();
-
-const oppgaverespons = [
-    {
-        oppgaveUuid: '234212',
-        ident: '12345645613',
-        opprettetDato: '2019-02-07T13:51:02',
-        oppgavetype: 'Sykemelding',
-        ressursId: '12355321,',
-        ressurseier: '123521',
-    },
-    {
-        oppgaveUuid: '334212',
-        ident: '3225645613',
-        opprettetDato: '2019-02-07T13:51:02',
-        oppgavetype: 'Sykepengesøknad',
-        ressursId: '12355321,',
-        ressurseier: '123521',
-    },
-];
-
-fetchMock
-    .get(
-        '/min-side-arbeidsgiver/api/syfooppgaver',
-        delay.then(() => {
-            return oppgaverespons;
         })
     )
     .spy();
