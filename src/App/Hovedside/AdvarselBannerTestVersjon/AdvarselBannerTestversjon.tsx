6b20ac3bc04fab0b96545d444603f63d429f469f
Ibenholt <bendik.segrov.ibenholt@nav.no>
2020-11-16 10:37:34 +0100

    tekstendringer

--- src/App/Hovedside/AdvarselBannerTestVersjon/AdvarselBannerTestversjon.tsx
+++ src/App/Hovedside/AdvarselBannerTestVersjon/AdvarselBannerTestversjon.tsx
@@ -10,8 +10,8 @@ const AdvarselBannerTestversjon = () => {
                 Her kan du bli bedre kjent med Min side – Arbeidsgiver.
                 <br/>
                 Testversjonen kan avvike noe fra den virkelige nettsiden og
-                foreløpig er det bare lenkene til <i>mine sykemeldte</i>,
-                og <i>tiltak</i> som virker. De andre sidene har ikke fungerende testversjoner.
+                foreløpig er det bare lenkene til <i>Sykemeldte</i>,
+                og <i>Tiltak</i> som virker. De andre sidene har ikke fungerende testversjoner.
                 <br/>
             </AlertStripeAdvarsel>
      }
