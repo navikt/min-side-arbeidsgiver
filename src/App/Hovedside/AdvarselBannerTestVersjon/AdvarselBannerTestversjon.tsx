bdc38fb879b2b2379423c06dba8b96178856375d
Ibenholt <bendik.segrov.ibenholt@nav.no>
2020-11-16 10:12:28 +0100

    la til infor om hvilke tjenester som ikke virker

--- src/App/Hovedside/AdvarselBannerTestVersjon/AdvarselBannerTestversjon.tsx
+++ src/App/Hovedside/AdvarselBannerTestVersjon/AdvarselBannerTestversjon.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import {AlertStripeAdvarsel} from "nav-frontend-alertstriper";
-
+import './AdvarselBannerTestversjon.less'
 const AdvarselBannerTestversjon = () => {
     return (
-        <>{window.location.hostname.includes('labs.nais.io') && (
+        <>{
             <AlertStripeAdvarsel className={'advarsel-banner-testversjon'}>
                 <b>Dette er en testversjon</b>
                 <br/>
                 Her kan du bli bedre kjent med Min side – Arbeidsgiver.
                 <br/>
+                Testversjonen kan avvike noe fra den virkelige nettsiden og
+                foreløpig er det bare lenkene til <i>mine sykemeldte</i>,
+                og <i>arbeidsavtaler</i> som virker, de andre sidene har ikke fungerende testversjoner.
+                <br/>
             </AlertStripeAdvarsel>
-        )}
+     }
         </>
     );
 };
