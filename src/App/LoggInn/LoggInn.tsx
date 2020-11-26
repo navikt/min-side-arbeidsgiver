1483e8f7e3b78cdb02a2aac30705778db3dfce24
Hilde Steinbru Heggstad <hilde.steinbru.heggstad@nav.no>
2020-11-26 17:21:28 +0100

    lagt inn logikk for nedetid

--- src/App/LoggInn/LoggInn.tsx
+++ src/App/LoggInn/LoggInn.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useEffect, useState } from "react";
 import { Hovedknapp } from 'nav-frontend-knapper';
 import { Normaltekst, Systemtittel } from 'nav-frontend-typografi';
 import Lenke from 'nav-frontend-lenker';
@@ -9,7 +9,24 @@ import Brodsmulesti from '../Brodsmulesti/Brodsmulesti';
 import './Logginn.less';
 import AdvarselBannerTestversjon from '../Hovedside/AdvarselBannerTestVersjon/AdvarselBannerTestversjon';
 
+
+//tidspunkt skirves på formen (2015-03-25T12:00:00Z"); blir f.eks klokka 13.00 ( 12:00 + en time = 13.00 pga tidsonen i Norge
+
+const advarselboksSettesPåString = '';
+const nedetidboksSettesPaString = ''
+
+export const regnUtOmBoksSkalVises =(tidspunkt: Date) => {
+    const nåVærendeTidspunkt = new Date();
+    return nåVærendeTidspunkt > tidspunkt;
+}
+
 export const LoggInn: FunctionComponent = () => {
+    const [visAdvarselBoks, setVisAdvarselBoks] = useState(false)
+    const [visNedetid, setVisNedetid] = useState(false)
+
+    const advarselboksSettesPåString = '';
+    const nedetidboksSettesPaString = ''
+
     const redirectTilLogin = () => {
         if (environment.MILJO === 'prod-sbs' || environment.MILJO === 'dev-sbs'||environment.MILJO === 'labs-gcp') {
             window.location.href = '/min-side-arbeidsgiver/redirect-til-login';
@@ -18,6 +35,27 @@ export const LoggInn: FunctionComponent = () => {
             window.location.href = '/min-side-arbeidsgiver';
         }
     };
+
+    useEffect(() => {
+        if (advarselboksSettesPåString.length >0  && nedetidboksSettesPaString.length>0)  {
+            const tidForAdvarselBoks = new Date(advarselboksSettesPåString);
+            const tidForNedetidBoks = new Date(nedetidboksSettesPaString);
+            const nedetidVises = regnUtOmBoksSkalVises(tidForNedetidBoks)
+            setVisAdvarselBoks(nedetidVises)
+            if (!nedetidVises) {
+                if (regnUtOmBoksSkalVises(tidForAdvarselBoks)){
+                    setVisNedetid(true)
+                }
+                else {
+                    setVisNedetid(false)
+                }
+            }
+
+        }
+    }, []);
+
+
+
     return (
         <div className="innloggingsside">
             <Brodsmulesti brodsmuler={[]} />
@@ -27,7 +65,6 @@ export const LoggInn: FunctionComponent = () => {
                 <Systemtittel className="innloggingsside__sidetittel">
                     På Min side – arbeidsgiver kan du:
                 </Systemtittel>
-
                 <ul className="innloggingsside__punktliste">
                     <li className="innloggingsside__punkt">
                         få oversikt over dine sykmeldte
