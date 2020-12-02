5023e6a51fdeb063f498b4845b5114b12c4bd0ad
Terese Haug <terese.haug@nav.no>
2020-12-02 08:33:38 +0100

    Fikser feil navn på variabler (jsdom variabelnavn)

--- src/server/decorator.js
+++ src/server/decorator.js
@@ -1,6 +1,6 @@
 const jsdom = require('jsdom');
 const request = require('request');
-const NodeCache = require("node-cache");
+const NodeCache = require('node-cache');
 const { JSDOM } = jsdom;
 
 const SECONDS_PER_MINUTE = 60;
@@ -12,30 +12,29 @@ const cache = new NodeCache({
     checkperiod: SECONDS_PER_MINUTE,
 });
 
-url = '';
-if (process.env.NAIS_CLUSTER_NAME === "prod-sbs") {
-    url = "https://www.nav.no/dekoratoren/?context=arbeidsgiver&redirectToApp=true&chatbot=true&level=Level4";
-} else {
-    url =  process.env.DECORATOR_EXTERNAL_URL || "https://www.nav.no/dekoratoren/?context=arbeidsgiver&redirectToApp=true&chatbot=true&level=Level4";
-}
+const url =
+    process.env.NAIS_CLUSTER_NAME === 'prod-sbs'
+    ? 'https://www.nav.no/dekoratoren/?context=arbeidsgiver&redirectToApp=true&chatbot=true&level=Level4'
+    : process.env.DECORATOR_EXTERNAL_URL || 'https://www.nav.no/dekoratoren/?context=arbeidsgiver&redirectToApp=true&chatbot=true&level=Level4'
+;
 
 const getDecorator = () =>
     new Promise((resolve, reject) => {
-        const cacheData = cache.get("main-cache");
+        const cacheData = cache.get('main-cache');
         if (cacheData) {
             resolve(cacheData);
         } else {
             request(url, (error, response, body) => {
                 if (!error && response.statusCode >= 200 && response.statusCode < 400) {
                     const { document } = new JSDOM(body).window;
-                    const prop = "innerHTML";
+                    const prop = 'innerHTML';
                     const data = {
-                        HEADER: document.getElementById("header-withmenu")[prop],
-                        STYLES: document.getElementById("styles")[prop],
-                        FOOTER: document.getElementById("footer-withmenu")[prop],
-                        SCRIPTS: document.getElementById("scripts")[prop],
+                        HEADER: document.getElementById('header-withmenu')[prop],
+                        FOOTER: document.getElementById('footer-withmenu')[prop],
+                        STYLES: document.getElementById('styles')[prop],
+                        SCRIPTS: document.getElementById('scripts')[prop],
                     };
-                    cache.set("main-cache", data);
+                    cache.set('main-cache', data);
                     console.log(`Creating cache`);
                     resolve(data);
                 } else {
