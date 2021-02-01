47dc3b7f162a86f8530d8f386b9856e2b55f4751
Terese Haug <terese.haug@nav.no>
2021-02-01 10:34:29 +0100

    Nytt ikon i Koronaboks

--- src/App/Koronaboks/Koronaboks.tsx
+++ src/App/Koronaboks/Koronaboks.tsx
@@ -1,29 +1,29 @@
 import React, { useContext } from 'react';
-import Innholdsboks from '../Hovedside/Innholdsboks/Innholdsboks';
+import { Undertittel } from 'nav-frontend-typografi';
 import Element from 'nav-frontend-typografi/lib/element';
 import Lenke from 'nav-frontend-lenker';
 import HoyreChevron from 'nav-frontend-chevron/lib/hoyre-chevron';
-import sirkel from './report-problem-circle.svg';
+import { OrganisasjonsDetaljerContext } from '../OrganisasjonDetaljerProvider';
 import {
     lenkeTilPermitteringOgMasseoppsigelsesSkjema,
     lenkeTilKlageskjema,
-    LenkeTilKoronaSykeRefusjon, lenkeTilPermitteringsInfo
+    LenkeTilKoronaSykeRefusjon,
+    lenkeTilPermitteringsInfo,
 } from '../../lenker';
-import { Undertittel } from 'nav-frontend-typografi';
-import { OrganisasjonsDetaljerContext } from '../OrganisasjonDetaljerProvider';
+import Innholdsboks from '../Hovedside/Innholdsboks/Innholdsboks';
+import KoronaboksIkon from './KoronaboksIkon';
 import './Koronaboks.less';
 
 export const Koronaboks = () => {
     const { valgtOrganisasjon } = useContext(OrganisasjonsDetaljerContext);
 
-    const harTilgangRefusjon =
-        valgtOrganisasjon && valgtOrganisasjon.altinntilgang.inntektsmelding;
+    const harTilgangRefusjon = valgtOrganisasjon && valgtOrganisasjon.altinntilgang.inntektsmelding;
     const orgnr = valgtOrganisasjon?.organisasjon.OrganizationNumber;
 
     return (
         <div className="koronaboks">
             <Innholdsboks classname="koronaboks__innhold">
-                <img className="koronaboks__ikon" alt="" src={sirkel} />
+                <KoronaboksIkon />
                 <Undertittel>Koronarelaterte tjenester</Undertittel>
 
                 <Element className="koronaboks__tekst">Permittering</Element>
@@ -38,22 +38,14 @@ export const Koronaboks = () => {
                     </span>
                     <HoyreChevron />
                 </Lenke>
-                <Lenke
-                  className="koronaboks__lenke"
-                  href={lenkeTilPermitteringsInfo}
-                >
-                    <span>
-                        Nye regler for lønnsplikt ved permittering
-                    </span>
+                <Lenke className="koronaboks__lenke" href={lenkeTilPermitteringsInfo}>
+                    <span>Nye regler for lønnsplikt ved permittering</span>
                     <HoyreChevron />
                 </Lenke>
 
                 {harTilgangRefusjon && orgnr && (
                     <>
-                        <Lenke
-                            className="koronaboks__lenke"
-                            href={lenkeTilKlageskjema(orgnr)}
-                        >
+                        <Lenke className="koronaboks__lenke" href={lenkeTilKlageskjema(orgnr)}>
                             <span>
                                 Endring av opplysninger/klage på vedtak for refusjon av lønn ved
                                 permittering
