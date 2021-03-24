3e981dbcb49a9584bf04ba71575678ab892a6c35
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2021-03-24 09:57:16 +0100

    vis lenker kun for dem som kan gjøre noe med dem

--- src/App/Koronaboks/Koronaboks.tsx
+++ src/App/Koronaboks/Koronaboks.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { FunctionComponent, useContext } from 'react';
 import { Undertittel } from 'nav-frontend-typografi';
 import Element from 'nav-frontend-typografi/lib/element';
 import Lenke from 'nav-frontend-lenker';
@@ -16,9 +16,6 @@ import './Koronaboks.less';
 
 export const Koronaboks = () => {
     const { valgtOrganisasjon } = useContext(OrganisasjonsDetaljerContext);
-
-    const harTilgangRefusjon = valgtOrganisasjon && valgtOrganisasjon.altinntilgang.inntektsmelding;
-    const orgnr = valgtOrganisasjon?.organisasjon.OrganizationNumber;
 
     return (
         <div className="koronaboks">
@@ -42,32 +39,33 @@ export const Koronaboks = () => {
                     <span>Nye regler for lønnsplikt ved permittering</span>
                     <HoyreChevron />
                 </Lenke>
-                <Lenke className="koronaboks__lenke" href={lenkeTilLonnskompensasjonOgRefusjon}>
-                    <span>Lønnskompensasjon og refusjon – se kvittering</span>
-                    <HoyreChevron />
-                </Lenke>
-
-                {harTilgangRefusjon && orgnr && (
-                    <>
-                        <Lenke className="koronaboks__lenke" href={lenkeTilKlageskjema(orgnr)}>
-                            <span>
-                                Endring av opplysninger/klage på vedtak for refusjon av lønn ved
-                                permittering
-                            </span>
-                            <HoyreChevron />
-                        </Lenke>
-
-                        <Element className="koronaboks__tekst">Refusjon sykepenger</Element>
-                        <Lenke
-                            className="koronaboks__lenke"
-                            href={LenkeTilKoronaSykeRefusjon(orgnr)}
-                        >
-                            <span>Søk om refusjon av sykepenger relatert til koronavirus</span>
-                            <HoyreChevron />
-                        </Lenke>
-                    </>
-                )}
+                {
+                    valgtOrganisasjon?.altinntilgang.inntektsmelding.tilgang === 'ja'
+                        ? <LenkerSomKreverInntekstmeldingtilgang orgnr={valgtOrganisasjon.organisasjon.OrganizationNumber} />
+                        : null
+                }
             </Innholdsboks>
         </div>
     );
 };
+const LenkerSomKreverInntekstmeldingtilgang: FunctionComponent<{orgnr: string}> = ({orgnr}) => <>
+    <Lenke className="koronaboks__lenke" href={lenkeTilLonnskompensasjonOgRefusjon}>
+        <span>Lønnskompensasjon og refusjon – se kvittering</span>
+        <HoyreChevron />
+    </Lenke>
+
+    <Lenke className="koronaboks__lenke" href={lenkeTilKlageskjema(orgnr)}>
+        <span>
+            Endring av opplysninger/klage på vedtak for refusjon av lønn ved
+            permittering
+        </span>
+        <HoyreChevron />
+    </Lenke>
+
+    <Element className="koronaboks__tekst">Refusjon sykepenger</Element>
+    <Lenke className="koronaboks__lenke" href={LenkeTilKoronaSykeRefusjon(orgnr)}>
+        <span>Søk om refusjon av sykepenger relatert til koronavirus</span>
+        <HoyreChevron />
+    </Lenke>
+</>;
+
