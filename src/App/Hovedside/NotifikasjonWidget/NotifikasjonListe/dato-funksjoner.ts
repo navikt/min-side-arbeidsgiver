ef615a736ff6c086663979e1e725ce2d7afd5039
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2021-08-03 13:14:20 +0200

    liten forbedring dato

--- src/App/Hovedside/NotifikasjonWidget/NotifikasjonListe/dato-funksjoner.ts
+++ src/App/Hovedside/NotifikasjonWidget/NotifikasjonListe/dato-funksjoner.ts
@@ -11,23 +11,22 @@ const today = new Date();
 const yesterday = new Date();
 yesterday.setDate(yesterday.getDate() - 1);
 
-export const isToday = (date: Date): boolean =>
+const isToday = (date: Date): boolean =>
     formatterDato(date) === formatterDato(today);
 
-export const isYesterday = (date: Date): boolean =>
+const isYesterday = (date: Date): boolean =>
     formatterDato(date) === formatterDato(yesterday);
 
-export const klokkeslett = (dato: Date) => {
+const klokkeslett = (dato: Date) => {
     const hour = dato.getHours().toString().padStart(2, '0');
     const minute = dato.getMinutes().toString().padStart(2, '0');
     return hour + '.' + minute;
 };
 
-export const datotekst = (date: Date, capitalize = false) => {
-    const dato = new Date(date);
+export const datotekst = (dato: Date, capitalize = false) => {
     if (isToday(dato)) {
         return (capitalize ? 'I' : 'i') + ' dag ' + klokkeslett(dato);
-    } else if (isYesterday(date)) {
+    } else if (isYesterday(dato)) {
         return (capitalize ? 'I' : 'i') + ' går ' + klokkeslett(dato);
     } else {
         return formatterDato(dato);
