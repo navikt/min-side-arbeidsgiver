d289d8d7d3b3734f6152547bda04906a4a589ef8
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2021-09-01 14:11:41 +0200

    legg til krav om refusjon av sykepenger

--- src/App/Hovedside/SkjemaveilederContainer/SkjemaveilederContainer.tsx
+++ src/App/Hovedside/SkjemaveilederContainer/SkjemaveilederContainer.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Undertittel } from 'nav-frontend-typografi';
 import { skjemaForArbeidsgiverURL } from '../../../lenker';
 import './SkjemaveilederContainer.less';
 import Lenke from 'nav-frontend-lenker';
 import { HoyreChevron } from 'nav-frontend-chevron';
 import Lenkepanel from 'nav-frontend-lenkepanel';
+import { OrganisasjonsDetaljerContext } from '../../OrganisasjonDetaljerProvider';
 
 export const SkjemaveilederContainer = () => {
+    const {valgtOrganisasjon} = useContext(OrganisasjonsDetaljerContext);
+    const tilgangInntektsmelding = valgtOrganisasjon?.altinntilgang?.inntektsmelding?.tilgang === 'ja';
+
     return (
         <div className="skjemaveileder-container">
             <Undertittel id="skjemaveileder-tittel" className="skjemaveileder-tittel">
@@ -25,6 +29,23 @@ export const SkjemaveilederContainer = () => {
                         Fritak fra arbeidsgiverperioden - kronisk sykdom
                     </Lenkepanel>
                 </li>
+
+                { tilgangInntektsmelding ?
+                    <>
+                        <li>
+                            <Lenkepanel tittelProps="element" href="https://arbeidsgiver.nav.no/fritak-agp/nb/gravid/krav">
+                                Refusjon av sykepenger i arbeidsgiverperioden - gravid ansatt
+                            </Lenkepanel>
+                        </li>
+
+                        <li>
+                            <Lenkepanel tittelProps="element" href="https://arbeidsgiver.nav.no/fritak-agp/nb/kronisk/krav">
+                                Refusjon av sykepenger i arbeidsgiverperioden - kronisk sykdom
+                            </Lenkepanel>
+                        </li>
+                    </>
+                    : null
+                }
             </ul>
 
 
