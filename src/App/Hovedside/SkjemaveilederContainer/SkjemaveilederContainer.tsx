73c8f32c573355089e63605378e9fbaa23279d56
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2021-09-01 14:03:07 +0200

    legger til lenker til nye skjema for kronisk syke og gravide (#761)

--- src/App/Hovedside/SkjemaveilederContainer/SkjemaveilederContainer.tsx
+++ src/App/Hovedside/SkjemaveilederContainer/SkjemaveilederContainer.tsx
@@ -1,25 +1,38 @@
 import React from 'react';
 import { Undertittel } from 'nav-frontend-typografi';
 import { skjemaForArbeidsgiverURL } from '../../../lenker';
-import AltinnLenke from '../AltinnContainer/AltinnLenke/AltinnLenke';
 import './SkjemaveilederContainer.less';
+import Lenke from 'nav-frontend-lenker';
+import { HoyreChevron } from 'nav-frontend-chevron';
+import Lenkepanel from 'nav-frontend-lenkepanel';
 
 export const SkjemaveilederContainer = () => {
     return (
         <div className="skjemaveileder-container">
             <Undertittel id="skjemaveileder-tittel" className="skjemaveileder-tittel">
-                Alle søknader og skjemaer
+                Søknader og skjemaer
             </Undertittel>
-            <ul aria-labelledby="skjemaveileder-tittel">
-                <AltinnLenke
-                    href={skjemaForArbeidsgiverURL}
-                    tekst="Sende skjema eller ettersende dokumenter"
-                    className="skjemaveileder-lenkepanel"
-                    nyFane={false}
-                >
-                    Sende skjema eller ettersende dokumenter
-                </AltinnLenke>
+
+            <ul>
+                <li>
+                    <Lenkepanel tittelProps="element" href="https://arbeidsgiver.nav.no/fritak-agp/nb/gravid/soknad">
+                        Fritak fra arbeidsgiverperioden - gravid ansatt
+                    </Lenkepanel>
+                </li>
+
+                <li>
+                    <Lenkepanel tittelProps="element" href="https://arbeidsgiver.nav.no/fritak-agp/nb/kronisk/soknad">
+                        Fritak fra arbeidsgiverperioden - kronisk sykdom
+                    </Lenkepanel>
+                </li>
             </ul>
+
+
+            <Lenke href={skjemaForArbeidsgiverURL}>
+                Alle søknader og skjemaer
+                <HoyreChevron />
+            </Lenke>
+
         </div>
     );
 };
