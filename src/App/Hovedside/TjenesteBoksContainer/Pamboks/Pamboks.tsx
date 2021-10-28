396b1556d7dad74f5357cf67016edd5105986a81
Bendik Ibenholt <bendik.segrov.ibenholt@nav.no>
2021-10-28 13:31:52 +0200

    Nytt design rekruttering (#856)
    * oppdater lenker til arbeidsplasse i dev
    
    * ny styling på arberidsplassenboks

--- src/App/Hovedside/TjenesteBoksContainer/Pamboks/Pamboks.tsx
+++ src/App/Hovedside/TjenesteBoksContainer/Pamboks/Pamboks.tsx
@@ -1,14 +1,27 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { Normaltekst } from 'nav-frontend-typografi';
 import { OrganisasjonsDetaljerContext } from '../../../OrganisasjonDetaljerProvider';
 import { arbeidsplassenURL } from '../../../../lenker';
 import TjenesteBoksBanner from '../TjenesteBoksBanner/TjenesteBoksBanner';
 import PamboksIkon from './pamboks-ikon.svg';
 import { LenkepanelMedLogging } from '../../../../GeneriskeElementer/LenkepanelMedLogging';
+import './Pamboks.less';
+
 
 const Pamboks = () => {
     const { antallAnnonser } = useContext(OrganisasjonsDetaljerContext);
     const [stillingsAnnonseTekst, setStillingsAnnonseTekst] = useState('Lag ny stillingsannonse');
+
+    const TekstUtenTall = () =>
+        <>Finn nye kandidater
+            <br />Lag en stillingsannonse</>;
+
+    const TekstMedTall = () =>
+        <div className={'bunntekst'}>
+            <span> <span className={'antall'}>{antallAnnonser}</span>stillingsannonser (aktive)</span>
+            <div className={'bunntekst'}>Finn nye kandidater
+            </div>
+        </div>;
+
 
     useEffect(() => {
         if (antallAnnonser > 0) {
@@ -17,18 +30,17 @@ const Pamboks = () => {
     }, [antallAnnonser]);
 
     return (
-        <div className="pamboks tjenesteboks-innhold">
-            <TjenesteBoksBanner tittel="Rekruttere" imgsource={PamboksIkon} altTekst="" />
+        <div className='pamboks tjenesteboks-innhold'>
+            <TjenesteBoksBanner tittel='Rekruttere' imgsource={PamboksIkon} altTekst='' />
             <LenkepanelMedLogging
-                className="pamboks__lenke"
-                loggLenketekst="Rekruttere"
+                className='pamboks__lenke'
+                loggLenketekst='Rekruttere'
                 href={arbeidsplassenURL}
-                tittelProps="normaltekst"
+                tittelProps='normaltekst'
                 aria-label={'Rekruttere, finn kandidater, ' + stillingsAnnonseTekst}
             >
-                <div className="pamboks-tekst">
-                    <Normaltekst>Finn kandidater</Normaltekst>
-                    {stillingsAnnonseTekst}
+                <div className='pamboks-tekst'>
+                    {antallAnnonser > 0 ? TekstMedTall() : TekstUtenTall()}
                 </div>
             </LenkepanelMedLogging>
         </div>
