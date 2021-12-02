345cd9d3ae81272d54c274cd348f529dcbadd575
Bendik Ibenholt <bendik.segrov.ibenholt@nav.no>
2021-12-02 10:43:07 +0100

    Klarerere tekst pmøtebooking (#944)
    * gjør teksten litt klarerer på møtebookings banneret

--- src/App/Hovedside/GiOssTilbakemeldingComponent/GiOssTilbakemelding.tsx
+++ src/App/Hovedside/GiOssTilbakemeldingComponent/GiOssTilbakemelding.tsx
@@ -30,7 +30,7 @@ export const GiOssTilbakemelding = () => {
         <Panel border
                className={'panel'}>
             <div className={'innhold'}>
-                <Normaltekst> Vi som lager siden vil gjerne høre om dine erfaringer som arbeidsgiver. <LenkeMedLogging
+                <Normaltekst> Er du arbeidsgiver og vil dele dine erfaringer med oss som lager tjenesten? <LenkeMedLogging
                     loggLenketekst={'Møtebooking'}
                     href={møteBookingLenke}>Avtal et digitalt møte med oss. </LenkeMedLogging> </Normaltekst>
                 <Lukknapp className={'lukk-knapp'} onClick={lukkOgSkrivTilLocalstorage}>Lukk</Lukknapp>
