38d46266aabb25a82fd3c4c3d40c305b3e64082d
bendiksi <bendik.segrov.ibenholt@nav.no>
2021-12-07 15:50:23 +0100

    funksjonsbryter tilbakemdlingsnoks, fiks unleash mock

--- server/mock/unleashMock.js
+++ server/mock/unleashMock.js
@@ -1,7 +1,7 @@
 module.exports = {
     mock: (app) => {
         app.use(
-            '/min-side-arbeidsgiver/api/feature?feature=msa.visMoteKalender',
+            '/min-side-arbeidsgiver/api/feature',
             (req, res) => res.send({"msa.visMoteKalender": true})
         );
     }
