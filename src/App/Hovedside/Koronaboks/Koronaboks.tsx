a820df6d3086f4961b4be47f620f98bd696fad2e
Ken Gullaksen <ken.gullaksen@nav.no>
2022-03-04 13:49:55 +0100

    TAG-1823: fiks layout og innhold iht skisse (#1170)
    * TAG-1823: fiks layout og innhold iht skisse
    
    * TAG-1823: tweak icon plassering
    
    * TAG-1823: alt i koronaboksen krever nå altinntilgang.inntektsmelding
    forenkler også komponenten litt
    
    * TAG-1823: fiks mas fra sonarcloud

--- src/App/Hovedside/Koronaboks/Koronaboks.tsx
+++ src/App/Hovedside/Koronaboks/Koronaboks.tsx
@@ -1,20 +1,18 @@
 import React, { FunctionComponent, useContext } from 'react';
-import { Undertittel } from 'nav-frontend-typografi';
 import Element from 'nav-frontend-typografi/lib/element';
 import HoyreChevron from 'nav-frontend-chevron/lib/hoyre-chevron';
 import { OrganisasjonsDetaljerContext } from '../../OrganisasjonDetaljerProvider';
 import {
-    lenkeTilPermitteringOgMasseoppsigelsesSkjema,
     permitteringKlageskjemaURL,
     koronaSykeRefusjonURL,
-    lenkeTilPermitteringsInfo, lenkeTilLonnskompensasjonOgRefusjon,
+    lenkeTilLonnskompensasjonOgRefusjon,
     grensekompURL,
     grensekompOversiktURL,
 } from '../../../lenker';
-import Innholdsboks from '../Innholdsboks/Innholdsboks';
 import KoronaboksIkon from './KoronaboksIkon';
 import './Koronaboks.less';
 import { LenkeMedLogging } from '../../../GeneriskeElementer/LenkeMedLogging';
+import Ekspanderbartpanel from "nav-frontend-ekspanderbartpanel";
 
 interface KoronalenkeProps {
     href: string;
@@ -32,64 +30,49 @@ const Koronalenke: FunctionComponent<KoronalenkeProps> = ({ href, tekst }) =>
     </LenkeMedLogging>;
 
 export const Koronaboks = () => {
-    const { valgtOrganisasjon } = useContext(OrganisasjonsDetaljerContext);
+    const {valgtOrganisasjon} = useContext(OrganisasjonsDetaljerContext);
 
-    return (
-        <div className='koronaboks'>
-            <Innholdsboks classname='koronaboks__innhold'>
-                <KoronaboksIkon />
-                <Undertittel>Koronarelaterte tjenester</Undertittel>
+    if (valgtOrganisasjon === undefined || !valgtOrganisasjon.altinntilgang.inntektsmelding) {
+        return null
+    }
 
-                <Element className='koronaboks__tekst'>Permittering</Element>
+    const orgnr = valgtOrganisasjon.organisasjon.OrganizationNumber
+    return (<Ekspanderbartpanel className='koronaboks'
+                                border={false}
+                                apen={true}
+                                tittel={
+                                    <><KoronaboksIkon/>Koronaspesifikke tjenester</>
+                                }>
+                <span className='koronaboks__innhold'>
+                    <Element className='koronaboks__tekst'>Refusjon sykepenger</Element>
+                    <Koronalenke
+                        href={koronaSykeRefusjonURL(orgnr)}
+                        tekst='Søk om refusjon av sykepenger ved koronavirus'
+                    />
 
-                <Koronalenke
-                    href={lenkeTilPermitteringOgMasseoppsigelsesSkjema}
-                    tekst='Varsle NAV om permitteringer, masseoppsigelser eller innskrenkninger i arbeidstiden'
-                />
+                    <Element className='koronaboks__tekst'>Lønnskompensasjon</Element>
+                    <Koronalenke
+                        href={lenkeTilLonnskompensasjonOgRefusjon}
+                        tekst='Se kvittering på innsendt skjema om lønnskompensasjon'
+                    />
 
-                <Koronalenke
-                    href={lenkeTilPermitteringsInfo}
-                    tekst='Nye regler for lønnsplikt ved permittering'
-                />
+                    <Koronalenke
+                        href={permitteringKlageskjemaURL(orgnr)}
+                        tekst='Ettersend opplysninger eller klag på vedtak om lønnskompensasjon'
+                    />
 
-                {
-                    valgtOrganisasjon?.altinntilgang.inntektsmelding === true
-                        ? <LenkerSomKreverInntekstmeldingtilgang
-                            orgnr={valgtOrganisasjon.organisasjon.OrganizationNumber} />
-                        : null
-                }
-            </Innholdsboks>
-        </div>
+                    <Element className='koronaboks__tekst'>Restriksjoner for innreise</Element>
+                    <Koronalenke
+                        href={grensekompURL}
+                        tekst='Søk om refusjon for utestengte EØS-borgere'
+                    />
+
+                    <Koronalenke
+                        href={grensekompOversiktURL}
+                        tekst='Refusjon for utestengte EØS-borgere - se innsendte krav'
+                    />
+                </span>
+        </Ekspanderbartpanel>
     );
 };
-const LenkerSomKreverInntekstmeldingtilgang: FunctionComponent<{ orgnr: string }> = ({ orgnr }) => <>
-
-    <Element className='koronaboks__tekst'>Lønnskompensasjon</Element>
-    <Koronalenke
-        href={lenkeTilLonnskompensasjonOgRefusjon}
-        tekst='Se kvittering på innsendt skjema om lønnskompensasjon'
-    />
-
-    <Koronalenke
-        href={permitteringKlageskjemaURL(orgnr)}
-        tekst='Ettersend opplysninger eller klag på vedtak om lønnskompensasjon'
-    />
-
-    <Element className='koronaboks__tekst'>Refusjon sykepenger</Element>
-    <Koronalenke
-        href={koronaSykeRefusjonURL(orgnr)}
-        tekst='Søk om refusjon av sykepenger ved koronavirus'
-    />
-
-    <Element className='koronaboks__tekst'>Restriksjoner for innreise</Element>
-    <Koronalenke
-        href={grensekompURL}
-        tekst='Søk om refusjon for utestengte EØS-borgere'
-    />
-
-    <Koronalenke
-        href={grensekompOversiktURL}
-        tekst='Refusjon for utestengte EØS-borgere - se innsendte krav'
-    />
-</>;
 
