becd39238e10c1494c447a09cce7fc564a9f0d89
ebelegu <89395215+ebelegu@users.noreply.github.com>
2022-03-08 11:12:17 +0100

    Update VarselHvisNedetid.tsx (#1175)
    Endret tidspunkt til ny aktuell nedetid.
    
    TAG-1856

--- src/App/LoggInn/VarselOmNedetid/VarselHvisNedetid.tsx
+++ src/App/LoggInn/VarselOmNedetid/VarselHvisNedetid.tsx
@@ -6,8 +6,8 @@ import { DisplayBetween } from '../../../GeneriskeElementer/DisplayBetween';
 
 
 export const VarselHvisNedetid: FunctionComponent = () => {
-    const showFrom = new Date('2021-05-14T00:00:00+02:00');
-    const showUntil = new Date('2021-05-15T10:00:00+02:00');
+    const showFrom = new Date('2022-03-08T19:00:00+02:00');
+    const showUntil = new Date('2022-03-09T06:00:00+02:00');
 
     return (
         <DisplayBetween showFrom={showFrom} showUntil={showUntil}>
@@ -17,7 +17,7 @@ export const VarselHvisNedetid: FunctionComponent = () => {
                         Tjenester for arbeidsgivere kan være utilgjengelig
                     </Element>
                     <p className="typo-normal">
-                        Fra i kveld (fredag 14. mai, klokken 21:00) til i morgen formiddag (lørdag 15. mai, klokken 10:00) vil de fleste innloggede tjenester for arbeidsgivere på nav.no være utilgjengelig.
+                        Fra i kveld (tirsdag 8. mars, klokken 23:00) til i morgen (9. mars, klokken 06:00) vil de fleste innloggede tjenester for arbeidsgivere på nav.no være utilgjengelig.
                     </p>
 
                     <p className="typo-normal">
