06d241e1c856ee3f3295ee7487b7d6a0aa699986
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2022-03-28 11:00:41 +0200

    Client side dekorator (#1197)
    * Bruk dekoratør injected på klient-siden
    
    * forenkl server.js

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,10 @@
 FROM navikt/node-express:16
 
 WORKDIR /usr/src/app
-
 COPY build/ build/
+COPY server/ server/
 
 WORKDIR /usr/src/app/server
-COPY server/ .
-
 USER root
 RUN npm ci
 USER apprunner
--- server/server.js
+++ server/server.js
@@ -1,34 +1,31 @@
 import path from 'path';
 import fetch from 'node-fetch';
 import express from 'express';
-import mustacheExpress from 'mustache-express';
+import Mustache from 'mustache';
 import httpProxyMiddleware from 'http-proxy-middleware';
-import jsdom from 'jsdom';
 import Prometheus from 'prom-client';
-import { createLogger, transports, format } from 'winston';
+import {createLogger, format, transports} from 'winston';
 import cookieParser from 'cookie-parser';
+import {readFileSync} from 'fs';
 import require from './esm-require.js';
 
 const apiMetricsMiddleware = require('prometheus-api-metrics');
-const { JSDOM } = jsdom;
 const { createProxyMiddleware } = httpProxyMiddleware;
 
 const defaultLoginUrl = 'http://localhost:8080/ditt-nav-arbeidsgiver-api/local/selvbetjening-login?redirect=http://localhost:3000/min-side-arbeidsgiver';
-const defaultDecoratorUrl = 'https://www.nav.no/dekoratoren/?context=arbeidsgiver&redirectToApp=true&chatbot=true&level=Level4';
 const {
     PORT = 3000,
     NAIS_APP_IMAGE = '?',
     GIT_COMMIT = '?',
     LOGIN_URL = defaultLoginUrl,
-    DECORATOR_EXTERNAL_URL = defaultDecoratorUrl,
     NAIS_CLUSTER_NAME = 'local',
     API_GATEWAY = 'http://localhost:8080',
-    DECORATOR_UPDATE_MS = 30 * 60 * 1000,
     PROXY_LOG_LEVEL = 'info',
     ARBEIDSFORHOLD_DOMAIN = 'http://localhost:8080',
     APIGW_TILTAK_HEADER,
     SYKEFRAVAER_DOMAIN = 'http://localhost:8080',
 } = process.env;
+
 const log = createLogger({
     transports: [
         new transports.Console({
@@ -38,48 +35,23 @@ const log = createLogger({
     ],
 });
 
-const decoratorUrl = NAIS_CLUSTER_NAME === 'prod-gcp' ? defaultDecoratorUrl : DECORATOR_EXTERNAL_URL;
 const BUILD_PATH = path.join(process.cwd(), '../build');
-const getDecoratorFragments = async () => {
-    const response = await fetch(decoratorUrl);
-    const body = await response.text();
-    const { document } = new JSDOM(body).window;
-    return {
-        HEADER: document.getElementById('header-withmenu').innerHTML,
-        FOOTER: document.getElementById('footer-withmenu').innerHTML,
-        STYLES: document.getElementById('styles').innerHTML,
-        SCRIPTS: document.getElementById('scripts').innerHTML,
+
+const indexHtml = Mustache.render(
+    readFileSync(path.join(BUILD_PATH, "index.html")).toString(),
+    {
         SETTINGS: `<script type="application/javascript">
             window.environment = {
                 MILJO: '${NAIS_CLUSTER_NAME}',
                 NAIS_APP_IMAGE: '${NAIS_APP_IMAGE}',
                 GIT_COMMIT: '${GIT_COMMIT}',
             }
-        </script>`,
-    };
-};
-const startApiGWGauge = () => {
-    const gauge = new Prometheus.Gauge({
-        name: 'backend_api_gw',
-        help: 'Status til backend via API-Gateway (sonekrysning). up=1, down=0',
-    });
-
-    setInterval(async () => {
-        try {
-            const res = await fetch(`${API_GATEWAY}/ditt-nav-arbeidsgiver-api/internal/actuator/health`);
-            gauge.set(res.ok ? 1 : 0);
-            log.info(`healthcheck: ${gauge.name} ${res.ok}`);
-        } catch (error) {
-            log.error(`healthcheck error: ${gauge.name} ${error}`);
-            gauge.set(0);
-        }
-    }, 60 * 1000);
-};
+        </script>`
+    }
+);
 
 const app = express();
 app.disable('x-powered-by');
-app.engine('html', mustacheExpress());
-app.set('view engine', 'mustache');
 app.set('views', BUILD_PATH);
 app.use(cookieParser());
 
@@ -93,8 +65,6 @@ app.use(
         metricsPath: '/min-side-arbeidsgiver/internal/metrics',
     }),
 );
-
-
 
 if (NAIS_CLUSTER_NAME === 'dev-gcp') {
     require('./mock/enhetsRegisteretMock').mock(app)
@@ -195,43 +165,29 @@ app.get(
     '/min-side-arbeidsgiver/internal/isReady',
     (req, res) => res.sendStatus(200),
 );
+app.get('/min-side-arbeidsgiver/*', (req, res) => {
+    res.send(indexHtml);
+});
 
-const serve = async () => {
-    let fragments;
+const gauge = new Prometheus.Gauge({
+    name: 'backend_api_gw',
+    help: 'Hvorvidt frontend-server naar backend-server. up=1, down=0',
+});
+
+setInterval(async () => {
     try {
-        fragments = await getDecoratorFragments();
-        app.get('/min-side-arbeidsgiver/*', (req, res) => {
-            res.render('index.html', fragments, (err, html) => {
-                if (err) {
-                    log.error(err);
-                    res.sendStatus(500);
-                } else {
-                    res.send(html);
-                }
-            });
-        });
-        app.listen(PORT, () => {
-            log.info(`Server listening on port ${PORT}`);
-        });
+        const res = await fetch(`${API_GATEWAY}/ditt-nav-arbeidsgiver-api/internal/actuator/health`);
+        gauge.set(res.ok ? 1 : 0);
     } catch (error) {
-        log.error(`Server failed to start ${error}`);
-        process.exit(1);
+        log.error(`healthcheck error: ${gauge.name} ${error}`);
+        gauge.set(0);
     }
+}, 60 * 1000);
 
-    startApiGWGauge();
-    setInterval(() => {
-        getDecoratorFragments()
-            .then(oppdatert => {
-                fragments = oppdatert;
-                log.info(`dekoratør oppdatert: ${Object.keys(oppdatert)}`);
-            })
-            .catch(error => {
-                log.warn(`oppdatering av dekoratør feilet: ${error}`);
-            });
-    }, DECORATOR_UPDATE_MS);
-};
+app.listen(PORT, () => {
+    log.info(`Server listening on port ${PORT}`);
+});
 
-serve().then(/*noop*/);
 
 if (NAIS_CLUSTER_NAME === 'labs-gcp') {
     import('@navikt/arbeidsgiver-notifikasjoner-brukerapi-mock');
