5b972c62a7343c650ad9f7a516f661349d125a28
ebelegu <89395215+ebelegu@users.noreply.github.com>
2022-03-28 09:55:44 +0200

    Satt opp søk, men full av mangler. (#1185)
    * Satt opp søk, men full av mangler.
    
    * bruk form med onsubmit og sett states.
    tenker vi nok bør ta i bruk en reducer som bygger opp variables til fetchmore når/da det blir flere variabler etterhvert
    
    * fisk sonar mas
    
    * mer sonar mas
    
    Co-authored-by: Ken Gullaksen <ken.gullaksen@nav.no>
    Co-authored-by: Peter Brottveit Bock <peter.brottveit.bock@nav.no>

--- src/App/Hovedside/Sak/Saksoversikt/Saksoversikt.tsx
+++ src/App/Hovedside/Sak/Saksoversikt/Saksoversikt.tsx
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect, useState} from 'react';
+import React, {useContext, useState} from 'react';
 import {gql, TypedDocumentNode, useQuery,} from '@apollo/client'
 import {GQL} from "@navikt/arbeidsgiver-notifikasjon-widget";
 import {OrganisasjonsDetaljerContext} from '../../../OrganisasjonDetaljerProvider';
@@ -8,11 +8,15 @@ import Lenke from "nav-frontend-lenker";
 import {Undertekst, UndertekstBold} from "nav-frontend-typografi";
 import Brodsmulesti from "../../../Brodsmulesti/Brodsmulesti";
 import SideBytter from "./SideBytter/SideBytter";
+import {Search} from "@navikt/ds-icons";
+import {SearchField} from "@navikt/ds-react";
+import SearchFieldButton from "@navikt/ds-react/esm/form/search-field/SearchFieldButton";
+import SearchFieldInput from "@navikt/ds-react/esm/form/search-field/SearchFieldInput";
 
 
 const HENT_SAKER: TypedDocumentNode<Pick<GQL.Query, "saker">> = gql`
-    query hentSaker($virksomhetsnummer: String!, $offset: Int, $limit: Int) {
-        saker(virksomhetsnummer: $virksomhetsnummer, offset: $offset, limit: $limit) {
+    query hentSaker($virksomhetsnummer: String!, $filter: String, $offset: Int, $limit: Int) {
+        saker(virksomhetsnummer: $virksomhetsnummer, filter: $filter, offset: $offset, limit: $limit) {
             saker {
                 id
                 tittel
@@ -46,21 +50,23 @@ const Saksoversikt = () => {
     const {loading, data, fetchMore} = useQuery(HENT_SAKER, {
         variables: {
             virksomhetsnummer: valgtOrganisasjon?.organisasjon?.OrganizationNumber,
+            filter: null,
             offset: 0,
             limit: sideStørrelse
         },
     })
-
-    const [valgtSide, settValgtSide] = useState(1);
-    useEffect(() => {
+    const oppdaterSaker = ({filter, side}: { filter: string, side: number }) => {
         const _ = fetchMore({
             variables: {
                 virksomhetsnummer: valgtOrganisasjon?.organisasjon?.OrganizationNumber,
-                offset: (valgtSide - 1) * sideStørrelse,
+                filter: filter !== "" ? filter : null,
+                offset: (side - 1) * sideStørrelse,
                 limit: sideStørrelse
             }
         });
-    }, [valgtSide]);
+    }
+    const [filter, settFilter] = useState("");
+    const [side, settSide] = useState(1);
 
     if (loading || !data || data?.saker.saker.length == 0) return null;
     const antallSider = Math.ceil(data?.saker.totaltAntallSaker / sideStørrelse)
@@ -70,14 +76,30 @@ const Saksoversikt = () => {
             <Brodsmulesti brodsmuler={[{ url: '/saksoversikt', title: 'Saksoversikt', handleInApp: true }]} />
 
             <div className="saksoversikt__header">
-                {/*<div className="saksoversikt__sokefelt">
-                    <TextField label="" placeholder="Søk" hideLabel />
-                    <Search height="1.5rem" width="1.5rem" className="saksoversikt__sokefelt-ikon"/>
-                </div>*/}
+                <div className="saksoversikt__sokefelt">
+                    <form onSubmit={(e)=>{
+                        settSide(1)
+                        oppdaterSaker({filter: filter, side: 1})
+                        e.preventDefault()
+                    }}>
+                    <SearchField  label='Søk' hideLabel>
+                        <SearchFieldInput value={filter} onChange={(e) => settFilter(e.target.value)}/>
+                        <SearchFieldButton variant="primary" type='submit'>
+                            <Search height="1.5rem" width="1.5rem" className="saksoversikt__sokefelt-ikon"/>
+                        </SearchFieldButton>
+                    </SearchField>
+                    </form>
+                    
+
+                </div>
 
                 <SideBytter
+                    side={side}
                     antallSider={antallSider}
-                    onSideValgt={settValgtSide}
+                    onSideValgt={(valgtSide) => {
+                        settSide(valgtSide)
+                        oppdaterSaker({filter, side: valgtSide})
+                    }}
                 />
             </div>
 
