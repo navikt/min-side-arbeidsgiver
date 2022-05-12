fa95630ef49f55ffc2c700373965b7e2272fddd4
ebelegu <egzon.belegu@nav.no>
2022-05-12 15:30:01 +0200

    Forenklet kode og fjernet gammel kode

--- src/App/Hovedside/SkjemaveilederContainer/SkjemaveilederContainer.tsx
+++ src/App/Hovedside/SkjemaveilederContainer/SkjemaveilederContainer.tsx
@@ -1,53 +1,31 @@
-import React, { useContext } from 'react';
-import { Undertittel } from 'nav-frontend-typografi';
+import React, {useContext} from 'react';
+import {Undertittel} from 'nav-frontend-typografi';
 import {lenkeTilPermitteringOgMasseoppsigelsesSkjema, skjemaForArbeidsgiverURL} from '../../../lenker';
 import './SkjemaveilederContainer.less';
-import { HoyreChevron } from 'nav-frontend-chevron';
-import { OrganisasjonsDetaljerContext } from '../../OrganisasjonDetaljerProvider';
-import { LenkeMedLogging } from '../../../GeneriskeElementer/LenkeMedLogging';
-import { LenkepanelMedLogging } from '../../../GeneriskeElementer/LenkepanelMedLogging';
+import {HoyreChevron} from 'nav-frontend-chevron';
+import {OrganisasjonsDetaljerContext} from '../../OrganisasjonDetaljerProvider';
+import {LenkeMedLogging} from '../../../GeneriskeElementer/LenkeMedLogging';
+import {LenkepanelMedLogging} from '../../../GeneriskeElementer/LenkepanelMedLogging';
 import {altinnskjema, AltinnskjemaId} from "../../../altinn/tjenester";
-import {OrganisasjonInfo} from "../../OrganisasjonerOgTilgangerProvider";
-import AltinnLenke from "../AltinnContainer/AltinnLenke/AltinnLenke";
 
 export const SkjemaveilederContainer = () => {
-    const { valgtOrganisasjon } = useContext(OrganisasjonsDetaljerContext);
+    const {valgtOrganisasjon} = useContext(OrganisasjonsDetaljerContext);
     const tilgangInntektsmelding = valgtOrganisasjon?.altinntilgang?.inntektsmelding;
-    const skjemarekkefølge: AltinnskjemaId[] = [
-        'inntektsmelding',
-        'mentortilskudd',
-        'inkluderingstilskudd',
-        'ekspertbistand',
-        'utsendtArbeidstakerEØS',
-    ];
-
-    function altinnSkjema(valgtOrganisasjon?: OrganisasjonInfo){
-        if (valgtOrganisasjon === undefined) {
-            return null;
-        }
-        const skjemaliste = skjemarekkefølge.flatMap(navn =>
-            valgtOrganisasjon.altinntilgang[navn] ? [altinnskjema[navn]] : [],
-        );
-
-        const str = skjemaliste.map(skjema => (
-            <li>
-                <LenkepanelMedLogging
-                    loggLenketekst={skjema.navn}
-                    tittelProps='element'
-                    href={skjema.skjemaUrl}
-                >
-                {skjema.navn } (Altinn)
-                </LenkepanelMedLogging>
-            </li>
-
-        ))
-        return str
+    if (valgtOrganisasjon === undefined) {
+        return null
     }
 
+    const altinnSkjemaLenke = (altinnSkjemaId: AltinnskjemaId) => {
+        if (!valgtOrganisasjon.altinntilgang[altinnSkjemaId]) {
+            return null;
+        }
+        const skjema = altinnskjema[altinnSkjemaId]
+        return lenke(skjema.navn, skjema.skjemaUrl, "_blank")
+    }
 
-    const lenke = (tekst: string, href: string) =>
+    const lenke = (tekst: string, href: string, target?: string) =>
         <li>
-            <LenkepanelMedLogging tittelProps='element' href={href} loggLenketekst={tekst}>
+            <LenkepanelMedLogging tittelProps='element' href={href} loggLenketekst={tekst} target={target}>
                 {tekst}
             </LenkepanelMedLogging>
         </li>;
@@ -88,17 +66,16 @@ export const SkjemaveilederContainer = () => {
                     'Varsle NAV om permitteringer, masseoppsigelser eller innskrenkninger i arbeidstiden',
                     lenkeTilPermitteringOgMasseoppsigelsesSkjema
                 )}
-
-
-
-            {
-                altinnSkjema(valgtOrganisasjon)
-            }
+                {altinnSkjemaLenke('inntektsmelding')}
+                {altinnSkjemaLenke('mentortilskudd')}
+                {altinnSkjemaLenke('inkluderingstilskudd')}
+                {altinnSkjemaLenke('ekspertbistand')}
+                {altinnSkjemaLenke('utsendtArbeidstakerEØS')}
             </ul>
 
             <LenkeMedLogging href={skjemaForArbeidsgiverURL} loggLenketekst='Alle søknader og skjemaer hos NAV'>
                 Alle søknader og skjemaer
-                <HoyreChevron />
+                <HoyreChevron/>
             </LenkeMedLogging>
         </div>
     );
