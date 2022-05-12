0e712786b75aef4a9977ae12741e11f73c0393ce
ebelegu <egzon.belegu@nav.no>
2022-05-12 11:26:28 +0200

    Lagt inn grid og fikset css.
    Må fjerne Søknader og skjemaer på Altinn og fikse ny fane for '(Altinn)'-skjemaene

--- src/App/Hovedside/SkjemaveilederContainer/SkjemaveilederContainer.tsx
+++ src/App/Hovedside/SkjemaveilederContainer/SkjemaveilederContainer.tsx
@@ -6,10 +6,44 @@ import { HoyreChevron } from 'nav-frontend-chevron';
 import { OrganisasjonsDetaljerContext } from '../../OrganisasjonDetaljerProvider';
 import { LenkeMedLogging } from '../../../GeneriskeElementer/LenkeMedLogging';
 import { LenkepanelMedLogging } from '../../../GeneriskeElementer/LenkepanelMedLogging';
+import {altinnskjema, AltinnskjemaId} from "../../../altinn/tjenester";
+import {OrganisasjonInfo} from "../../OrganisasjonerOgTilgangerProvider";
+import AltinnLenke from "../AltinnContainer/AltinnLenke/AltinnLenke";
 
 export const SkjemaveilederContainer = () => {
     const { valgtOrganisasjon } = useContext(OrganisasjonsDetaljerContext);
     const tilgangInntektsmelding = valgtOrganisasjon?.altinntilgang?.inntektsmelding;
+    const skjemarekkefølge: AltinnskjemaId[] = [
+        'inntektsmelding',
+        'mentortilskudd',
+        'inkluderingstilskudd',
+        'ekspertbistand',
+        'utsendtArbeidstakerEØS',
+    ];
+
+    function altinnSkjema(valgtOrganisasjon?: OrganisasjonInfo){
+        if (valgtOrganisasjon === undefined) {
+            return null;
+        }
+        const skjemaliste = skjemarekkefølge.flatMap(navn =>
+            valgtOrganisasjon.altinntilgang[navn] ? [altinnskjema[navn]] : [],
+        );
+
+        const str = skjemaliste.map(skjema => (
+            <li>
+                <LenkepanelMedLogging
+                    loggLenketekst={skjema.navn}
+                    tittelProps='element'
+                    href={skjema.skjemaUrl}
+                >
+                {skjema.navn } (Altinn)
+                </LenkepanelMedLogging>
+            </li>
+
+        ))
+        return str
+    }
+
 
     const lenke = (tekst: string, href: string) =>
         <li>
@@ -50,13 +84,17 @@ export const SkjemaveilederContainer = () => {
                     : null
                 }
 
-
                 {lenke(
                     'Varsle NAV om permitteringer, masseoppsigelser eller innskrenkninger i arbeidstiden',
                     lenkeTilPermitteringOgMasseoppsigelsesSkjema
                 )}
-            </ul>
 
+
+
+            {
+                altinnSkjema(valgtOrganisasjon)
+            }
+            </ul>
 
             <LenkeMedLogging href={skjemaForArbeidsgiverURL} loggLenketekst='Alle søknader og skjemaer hos NAV'>
                 Alle søknader og skjemaer
