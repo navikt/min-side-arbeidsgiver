20383a42bf46599f6c35e10c360caa8a3467d14a
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2022-05-27 10:04:17 +0200

    redirect med en gang

--- src/App/Hovedside/Sak/Saksoversikt/SaksoversiktRestoreSession.tsx
+++ src/App/Hovedside/Sak/Saksoversikt/SaksoversiktRestoreSession.tsx
@@ -1,15 +1,12 @@
 import { useRestoreSessionFromStorage } from './useOversiktSessionStorage';
-import { useValgtOrganisasjon } from '../../../OrganisasjonDetaljerProvider';
 import { useEffect } from 'react';
 
 export const SaksoversiktRestoreSession = () => {
-    const valgtOrganisasjon = useValgtOrganisasjon()
     const restore = useRestoreSessionFromStorage()
 
     useEffect(() => {
         restore()
-    }, [valgtOrganisasjon])
+    }, [])
 
-    console.log("SaksoversiktRestoreSession", {valgtOrganisasjon})
     return null
 }
