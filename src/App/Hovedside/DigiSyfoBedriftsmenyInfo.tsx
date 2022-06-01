cfa2a11a16bfa30b2bafd9200742f9c10c1bc811
Ken Gullaksen <ken.gullaksen@nav.no>
2022-06-01 11:55:22 +0200

    hack til lukkeknapp

--- src/App/Hovedside/DigiSyfoBedriftsmenyInfo.tsx
+++ src/App/Hovedside/DigiSyfoBedriftsmenyInfo.tsx
@@ -1,23 +1,35 @@
-import React, {FunctionComponent, useContext} from 'react';
+import React, {FunctionComponent, useContext, useState} from 'react';
 import {OrganisasjonerOgTilgangerContext, SyfoTilgang} from '../OrganisasjonerOgTilgangerProvider';
 import {Alert, BodyShort, Heading} from "@navikt/ds-react";
+import "./DigiSyfoBedriftsmenyInfo.less";
+import Lukknapp from "nav-frontend-lukknapp";
 
+const localStorageKey = 'DigiSyfoBedriftsmenyInfoLukket';
 export const DigiSyfoBedriftsmenyInfo: FunctionComponent = () => {
     const { tilgangTilSyfo } = useContext(OrganisasjonerOgTilgangerContext);
+    const [erLukketTidligere, setErLukketTidligere] = useState(window.localStorage.getItem(localStorageKey) != null);
 
-    if (tilgangTilSyfo !== SyfoTilgang.TILGANG) {
+    const lukkOgSkrivTilLocalstorage = () => {
+        window.localStorage.setItem(localStorageKey, new Date().toDateString());
+        setErLukketTidligere(true);
+    };
+
+    if (erLukketTidligere || tilgangTilSyfo !== SyfoTilgang.TILGANG) {
         return null;
     }
 
     return (
-        <Alert variant="info">
-            <Heading size="small" spacing={true}>
-                Sykemeldte er nå koblet til virksomhetsvelgeren
-            </Heading>
-            <BodyShort>
-                Velg virksomhet for å se sykemeldte du skal følge opp der. Boksen “Sykemeldte” vises bare når du har
-                noen sykemeldt å følge opp på valgt virksomhet.
-            </BodyShort>
-        </Alert>
+        <div className={"digisyfo-bedriftsmeny-info"}>
+            <Alert variant="info" className={"digisyfo-bedriftsmeny-info__alert"}>
+                <Heading size="small" spacing={true} >
+                    Sykemeldte er nå koblet til virksomhetsvelgeren
+                </Heading>
+                <BodyShort>
+                    Velg virksomhet for å se sykemeldte du skal følge opp der. Boksen “Sykemeldte” vises bare når du har
+                    noen sykemeldt å følge opp på valgt virksomhet.
+                </BodyShort>
+            </Alert>
+            <Lukknapp className={'lukk-knapp'} onClick={lukkOgSkrivTilLocalstorage}>Lukk</Lukknapp>
+        </div>
     );
 };
