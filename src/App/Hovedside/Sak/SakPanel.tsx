972af6b41337a7283819e92fdfb09fcb8980bc54
ebelegu <egzon.belegu@nav.no>
2022-06-30 13:24:34 +0200

    Reduserte linjehoyde i tekst og økte mellom div

--- src/App/Hovedside/Sak/SakPanel.tsx
+++ src/App/Hovedside/Sak/SakPanel.tsx
@@ -1,8 +1,9 @@
-import { BodyShort, LinkPanel } from '@navikt/ds-react';
+import {BodyShort, LinkPanel} from '@navikt/ds-react';
 import React from 'react';
-import { GQL } from '@navikt/arbeidsgiver-notifikasjon-widget';
+import {GQL} from '@navikt/arbeidsgiver-notifikasjon-widget';
 import {loggNavigasjon} from "../../../utils/funksjonerForAmplitudeLogging";
 import {useLocation} from "react-router-dom";
+import "./SaksListe.less"
 
 const dateFormat = new Intl.DateTimeFormat('no', {
     year: 'numeric',
@@ -18,19 +19,22 @@ type SakPanelProps = {
 export const SakPanel = ({placeholder, sak: {lenke, tittel, virksomhet, sisteStatus}}: SakPanelProps) => {
     const fake = placeholder ?? false
     const style: React.CSSProperties = fake ? {visibility: 'hidden'} : {}
-    const { pathname } = useLocation()
+    const {pathname} = useLocation()
     return <LinkPanel href={lenke} as={fake ? 'div' : 'a'} onClick={() => {
         loggNavigasjon(lenke, tittel, pathname)
     }}>
-        <BodyShort size="small" style={style}>
-            {virksomhet.navn.toUpperCase()}
-        </BodyShort>
+        <div className="sakscontent">
+            <BodyShort size="small" style={style}>
+                {virksomhet.navn.toUpperCase()}
+            </BodyShort>
 
-        <LinkPanel.Title style={{...style, fontSize: "1.1rem"}} >
-            {tittel}
-        </LinkPanel.Title>
-        <BodyShort size="small" style={style}>
-            {sisteStatus.tekst}{' '}{dateFormat.format(new Date(sisteStatus.tidspunkt))}
-        </BodyShort>
+            <LinkPanel.Title style={style} className="sakstekst">
+                {tittel}
+            </LinkPanel.Title>
+
+            <BodyShort size="small" style={style}>
+                {sisteStatus.tekst}{' '}{dateFormat.format(new Date(sisteStatus.tidspunkt))}
+            </BodyShort>
+        </div>
     </LinkPanel>
 }
