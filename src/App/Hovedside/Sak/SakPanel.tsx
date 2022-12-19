1a616622e6db8a2df5c4425386de0f6016cd6f6a
ebelegu <egzon.belegu@nav.no>
2022-12-19 14:40:02 +0100

    Påminnelse på sak

--- src/App/Hovedside/Sak/SakPanel.tsx
+++ src/App/Hovedside/Sak/SakPanel.tsx
@@ -5,6 +5,7 @@ import {loggNavigasjonTags} from '../../../utils/funksjonerForAmplitudeLogging';
 import {useLocation} from "react-router-dom";
 import "./SaksListe.css"
 import OppgaveIkon from "./OppgaveIkon";
+import {Oppgave, OppgaveTilstand} from "@navikt/arbeidsgiver-notifikasjon-widget/lib/cjs/lib/esm/api/graphql-types";
 
 const dateFormat = new Intl.DateTimeFormat('no', {
     year: 'numeric',
@@ -19,12 +20,13 @@ type SakPanelProps = {
 
 export const SakPanel = ({
     placeholder,
-    sak: {lenke, tittel, virksomhet, sisteStatus, merkelapp, frister}
+    sak: {lenke, tittel, virksomhet, sisteStatus, merkelapp, frister, oppgaver}
 }: SakPanelProps) => {
     const fake = placeholder ?? false
     const style: React.CSSProperties = fake ? {visibility: 'hidden'} : {}
     const {pathname} = useLocation()
     const [frist,] = frister
+    const påminnelse = oppgaver?.some((oppgave: Oppgave) => oppgave.tilstand === OppgaveTilstand.Ny && oppgave.paaminnelseTidspunkt !== null)
 
     return <LinkPanel className="sakslenkepanel" href={lenke} as={fake ? 'div' : 'a'} onClick={() => {
         loggNavigasjonTags(lenke, merkelapp, pathname, {component: 'sak'})
@@ -47,7 +49,10 @@ export const SakPanel = ({
             {frist !== undefined &&
                 <div className="saksfrist" style={style}>
                     <OppgaveIkon/>
-                    <BodyShort size="small"> Oppgave venter {frist == null ? "" : ` – frist ${dateFormat.format(new Date(frist))}`}</BodyShort>
+                    <div>
+                        {påminnelse??<BodyShort size="small" className="paminnelse"> Påminnelse </BodyShort>}
+                        <BodyShort size="small"> Oppgave venter {frist == null ? "" : ` – frist ${dateFormat.format(new Date(frist))}`}</BodyShort>
+                    </div>
                 </div>
             }
         </div>
