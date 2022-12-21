d283b1c6c8eb090172a11625fe8e9092bc885fbf
Ken Gullaksen <ken.gullaksen@nav.no>
2022-12-21 18:12:06 +0100

    fiks overflødig gulv

--- server/mock/antallArbeidsforholdMock.js
+++ server/mock/antallArbeidsforholdMock.js
@@ -2,7 +2,7 @@ module.exports = {
     mock: (app) => {
         app.use('/min-side-arbeidsgiver/api/antall-arbeidsforhold', (req, res) => {
             const antall = 502;
-            const missing = Math.floor(Math.random() * Math.floor(10)) === 0;
+            const missing = Math.floor(Math.random() * 10) === 0;
             setTimeout(() => {
                 res.send({first: '910811111', second: missing ? -1 : antall})
             }, 1000);
