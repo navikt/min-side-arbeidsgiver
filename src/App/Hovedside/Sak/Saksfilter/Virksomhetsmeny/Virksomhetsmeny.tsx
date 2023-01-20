0f679946b86bcea8da1073e54340f154d2be9e99
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2023-01-20 15:33:33 +0100

    handle click event outside

--- src/App/Hovedside/Sak/Saksfilter/Virksomhetsmeny/Virksomhetsmeny.tsx
+++ src/App/Hovedside/Sak/Saksfilter/Virksomhetsmeny/Virksomhetsmeny.tsx
@@ -118,11 +118,9 @@ const useOnClickOutside = (ref: React.RefObject<HTMLDivElement>, handler: (event
             }
             handler(event);
         };
-        document.addEventListener("mousedown", listener);
-        document.addEventListener("touchstart", listener);
+        document.addEventListener("click", listener);
         return () => {
-            document.removeEventListener("mousedown", listener);
-            document.removeEventListener("touchstart", listener);
+            document.removeEventListener("click", listener);
         };
     }, [ref, handler]);
 }
