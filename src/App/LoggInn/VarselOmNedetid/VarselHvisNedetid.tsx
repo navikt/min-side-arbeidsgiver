79bc1ec95f7a8a0048100cd4c3db696e2809bc4b
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2023-02-13 09:15:09 +0100

    remove driftsmelding

--- src/App/LoggInn/VarselOmNedetid/VarselHvisNedetid.tsx
+++ src/App/LoggInn/VarselOmNedetid/VarselHvisNedetid.tsx
@@ -1,26 +1,23 @@
 import React, { FunctionComponent } from 'react';
-import {Alert, BodyLong, BodyShort, Heading} from "@navikt/ds-react";
-import { DisplayBetween } from '../../../GeneriskeElementer/DisplayBetween';
 
 
 export const VarselHvisNedetid: FunctionComponent = () => {
     // const showFrom = new Date('2022-03-08T19:00:00+02:00');
     // const showUntil = new Date('2022-03-09T06:00:00+02:00');
 
-    return (
+    return null
         // <DisplayBetween showFrom={showFrom} showUntil={showUntil}>
-            <div className={'nedetid'}>
-                <Alert variant="warning" size="medium" className='nedetid__varsel'>
-                    <Heading spacing size="small">
-                        Driftsproblemer
-                    </Heading>
-                    <BodyLong>
-                        Min side – arbeidsgiver er ustabil, og det kan det være du ikke
-                        ser saker, beskjeder eller oppgaver du egentlig skal se.
-                        Prøv igjen senere.
-                    </BodyLong>
-                </Alert>
-            </div>
+        //     <div className={'nedetid'}>
+        //         <Alert variant="warning" size="medium" className='nedetid__varsel'>
+        //             <Heading spacing size="small">
+        //                 Driftsproblemer
+        //             </Heading>
+        //             <BodyLong>
+        //                 Min side – arbeidsgiver er ustabil, og det kan det være du ikke
+        //                 ser saker, beskjeder eller oppgaver du egentlig skal se.
+        //                 Prøv igjen senere.
+        //             </BodyLong>
+        //         </Alert>
+        //     </div>
         // </DisplayBetween>
-    );
 };
