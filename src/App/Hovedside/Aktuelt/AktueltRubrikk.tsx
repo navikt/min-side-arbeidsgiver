d05d567c0a1d62c7775c3fd667bfdf414c946f5f
ebelegu <egzon.belegu@nav.no>
2023-02-23 11:15:08 +0100

    Aktuell vises basert på tilgang

--- src/App/Hovedside/Aktuelt/AktueltRubrikk.tsx
+++ src/App/Hovedside/Aktuelt/AktueltRubrikk.tsx
@@ -1,9 +1,10 @@
 import {Heading, LinkPanel} from "@navikt/ds-react";
-import React from "react";
+import React, {useContext} from "react";
 import {loggNavigasjonTags} from "../../../utils/funksjonerForAmplitudeLogging";
 import {DisplayBetween, shouldDisplay} from "../../../GeneriskeElementer/DisplayBetween";
 import {useLocation} from "react-router-dom";
 import "./AktueltRubrikk.css"
+import {OrganisasjonsDetaljerContext} from "../../OrganisasjonDetaljerProvider";
 
 type AktueltProps = {
     lenke: string,
@@ -19,7 +20,20 @@ const dateFormat = new Intl.DateTimeFormat('no', {
 });
 
 const Aktuelt = ({lenke, tittel, visFra, visTil}: AktueltProps) => {
+    const {valgtOrganisasjon} = useContext(OrganisasjonsDetaljerContext);
     const {pathname} = useLocation()
+    if (!valgtOrganisasjon) {
+        return null
+    }
+
+    const skalVises = valgtOrganisasjon.altinntilgang.midlertidigLønnstilskudd ||
+        valgtOrganisasjon.altinntilgang.varigLønnstilskudd ||
+        (valgtOrganisasjon.altinntilgang.inntektsmelding && (valgtOrganisasjon.refusjonstatus["KLAR_FOR_INNSENDING"] ?? 0) > 0);
+
+    if (!skalVises) {
+        return null
+    }
+
     return (
         <DisplayBetween showFrom={visFra} showUntil={visTil}>
             <LinkPanel className="aktuelt-panel" href={lenke} border onClick={() => {
@@ -32,14 +46,15 @@ const Aktuelt = ({lenke, tittel, visFra, visTil}: AktueltProps) => {
             </LinkPanel>
         </DisplayBetween>
     );
+
 };
 
 const aktuelt: Array<AktueltProps> = [
     {
         lenke: "https://www.nav.no/no/bedrift/innhold-til-bedrift-forside/nyttig-a-vite/lonnstilskudd-far-ny-digital-refusjonslosning-fra-februar",
         tittel: "Lønnstilskudd får ny digital refusjonsløsning fra februar",
-        visFra: new Date('2023-01-11T00:00:00+02:00'),
-        visTil: new Date('2023-02-08T23:59:59+02:00'),
+        visFra: new Date('2023-02-23T00:00:00+02:00'),
+        visTil: new Date('2023-03-23T23:59:59+02:00'),
     },
 ]
 
@@ -60,11 +75,11 @@ export const AktueltRubrikk = () => {
         </Heading>
         <div className="aktuelt">
             {aktuelt.map(({
-                               lenke,
-                               tittel,
-                               visFra,
-                               visTil,
-                           }) =>
+                              lenke,
+                              tittel,
+                              visFra,
+                              visTil,
+                          }) =>
                 <Aktuelt key={tittel} lenke={lenke} tittel={tittel} visFra={visFra} visTil={visTil}/>
             )}
         </div>
