8cd8031778192db325149efaedd4ecf16458c9b8
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2023-04-12 14:10:57 +0200

    fjern ubrukte lenke, fiks labslenke til permitteringssskjema

--- src/lenker.ts
+++ src/lenker.ts
@@ -42,26 +42,11 @@ export const ringOssTLF = gittMiljo({
     other: "tel:00000000"
 })
 
-
 export const infoOmTilgangsstyringURL =
     'https://www.nav.no/arbeidsgiver/tilganger'
 
 export const infoOmNærmesteLederURL =
     'https://www.nav.no/no/bedrift/oppfolging/sykmeldt-arbeidstaker/digital-sykmelding-informasjon-til-arbeidsgivere/hvordan-melde-inn-naermeste-leder-for-en-sykmeldt_kap';
-
-export const infoOmRettigheterTilSykemeldingURL =
-    'https://www.nav.no/no/Bedrift/Oppfolging/Sykmeldt+arbeidstaker/digital-sykmelding-informasjon-til-arbeidsgivere/om-tilganger-i-altinn';
-
-export const infoOmAltinnVarslerURL =
-    'https://www.altinn.no/hjelp/profil/kontaktinformasjon/';
-
-export const spørreOmRettigheterAltinnURL=
-    'https://www.altinn.no/hjelp/profil/sporre-om-rettighet/';
-
-export const infoOmSykefraværsstatistikk =
-    'https://arbeidsgiver.nav.no/forebygge-sykefravaer/#digitale-tjenester';
-
-
 
 export const enhetsregisteretUnderenhetLink = (orgnr: string) =>
     `https://data.brreg.no/enhetsregisteret/oppslag/underenheter/${orgnr}`;
@@ -91,40 +76,17 @@ export const lenkeTilForebyggingsplan = gittMiljo({
     other: 'https://forebyggingsplan-frontend.dev.nav.no/forebyggingsplan',
 });
 
-export const lenkeTilInfoOmDigitaleSoknader =
-    'https://arbeidsgiver.nav.no/veiviserarbeidsgiver/tema/hvordan-kan-nav-hjelpe-med-inkludering';
-
-export const lenkeTilInforOmInntekstmelding =
-    'https://www.nav.no/no/bedrift/tjenester-og-skjemaer/nav-og-altinn-tjenester/inntektsmelding';
-
 export const lenkeTilPermitteringOgMasseoppsigelsesSkjema = gittMiljo({
     prod: 'https://arbeidsgiver.nav.no/permittering/',
-    demo: 'https://permitteringsskjema.labs.nais.io/permittering',
+    demo: 'https://arbeidsgiver.ekstern.dev.nav.no/permittering',
     other: 'https://arbeidsgiver-q.nav.no/permittering/',
 });
-
-const lenkeTilKlageskjemaBase = gittMiljo({
-    prod: 'https://arbeidsgiver.nav.no/klage-permittering-refusjon/',
-    other: 'https://arbeidsgiver-q.nav.no/klage-permittering-refusjon/',
-});
-
-export const permitteringKlageskjemaURL = (orgnr: string) =>
-    `${lenkeTilKlageskjemaBase}?bedrift=${orgnr}`;
 
 export const tiltaksgjennomforingURL = gittMiljo({
     prod: 'https://arbeidsgiver.nav.no/tiltaksgjennomforing/?part=arbeidsgiver',
     demo: 'https://arbeidsgiver.labs.nais.io/tiltaksgjennomforing/?part=arbeidsgiver',
     other: 'https://arbeidsgiver-q.nav.no/tiltaksgjennomforing/?part=arbeidsgiver',
 });
-
-export const infoOmPermitteringURL =
-    'https://www.nav.no/arbeidsgiver/permittere-nedbemanne';
-
-export const infoOmRefusjonSykepengerKoronaURL =
-    'https://www.nav.no/no/bedrift/oppfolging/sykmeldt-arbeidstaker/nyheter/refusjon-av-sykepenger-ved-koronavirus--hva-er-status#chapter-2';
-
-export const infoOmRefusjonInnreiseforbudKoronaURL =
-    'https://www.nav.no/no/bedrift/refusjon-ved-innreiseforbud-under-pandemien';
 
 
 
