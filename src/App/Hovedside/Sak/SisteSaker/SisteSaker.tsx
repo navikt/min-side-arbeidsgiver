751c0cb56e1165188ce3edee63f4d1afe23a0464
ebelegu <egzon.belegu@nav.no>
2023-04-24 20:22:22 +0200

    Siste saker ikke ferdig
    Bruker JS til hover fordi hover på andre en klasse ikke kan påvirke forrige søsken.

--- src/App/Hovedside/Sak/SisteSaker/SisteSaker.tsx
+++ src/App/Hovedside/Sak/SisteSaker/SisteSaker.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react';
+import React, { ReactNode, useContext, useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { OrganisasjonsDetaljerContext } from '../../../OrganisasjonDetaljerProvider';
 import './SisteSaker.css';
@@ -11,8 +11,67 @@ import { OmSaker } from '../OmSaker';
 import { useSessionStateForside } from '../Saksoversikt/useOversiktSessionStorage';
 import { SakSortering } from '../../../../api/graphql-types';
 import { FileFolder } from '@navikt/ds-icons';
+import { PaperplaneIcon, BellDotFillIcon } from '@navikt/aksel-icons';
 
 const ANTALL_FORSIDESAKER: number = 3;
+
+interface SakerLenkeProps {
+    ikon: ReactNode;
+    overskrift: string;
+    undertekst: string;
+}
+
+const SakerLenke = ({ ikon, overskrift, undertekst }: SakerLenkeProps) => {
+    const [hover, setHover] = useState(false);
+    const ikonId = "ikon-id" + overskrift.toLowerCase().replace(' ', '-');
+    const lenkeId = "lenke-id" + overskrift.toLowerCase().replace(' ', '-');
+
+    addEventListener('mouseover', (e) => {
+        const target = e.target as HTMLElement;
+        if (target.id === ikonId) {
+            setHover(true);
+        }
+        else if (target.id === lenkeId) {
+            setHover(true);
+        }
+        else {
+            setHover(false);
+        }
+    })
+    return <div className='saker-lenke'>
+        <Link
+            id={lenkeId}
+            className={'saker-lenke__ikon' + (hover ? ' saker-lenke__ikon__hover' : '')}
+            to={{
+                pathname: 'saksoversikt',
+                search: location.search,
+            }}
+            onClick={() => {
+                scroll(0, 0);
+                loggNavigasjon('saksoversikt', 'se alle saker', location.pathname);
+            }}
+        >
+            {ikon}
+        </Link>
+
+        <Link className={'saker-lenke_headerlenke' + (hover ? ' saker-lenke_headerlenke__hover' : '')}
+              to={{
+                  pathname: 'saksoversikt',
+                  search: location.search,
+              }}
+              onClick={() => {
+                  scroll(0, 0);
+                  loggNavigasjon('saksoversikt', 'se alle saker', location.pathname);
+              }}
+        >
+            <Heading id={ikonId} size={'small'}>
+                {overskrift}
+            </Heading>
+        </Link>
+        <BodyShort className='saker-lenke__undertekst'>{undertekst}</BodyShort>
+
+    </div>;
+};
 
 const SisteSaker = () => {
     const { valgtOrganisasjon, antallSakerForAlleBedrifter } = useContext(OrganisasjonsDetaljerContext);
@@ -59,33 +118,25 @@ const SisteSaker = () => {
             </div>
         </Link>;
     }
-
+    console.log(data.saker.sakstyper);
+    // @ts-ignore
     return (
-        <div className='innsynisak'>
-            <div className='innsynisak__header'>
-                <div className='innsynisak__tittel'>
-                    <Heading size='small' level='2'>
-                        Siste saker
-                    </Heading>
-                    <OmSaker />
+        <>
+            <div className='siste_saker'>
+                <Heading size='small' level='2'> Saker for dine virksomheter </Heading>
+                <div className='siste_saker_valg_container'>
+                    <SakerLenke
+                        ikon={<PaperplaneIcon title={`Antall saker (${antallSakerForAlleBedrifter})`} />}
+                        overskrift={`Antall saker (${antallSakerForAlleBedrifter})`}
+                        undertekst='Inntektsmelding, Permittering, Nedbemanning ...'
+                    />
+                    <SakerLenke
+                        ikon={<BellDotFillIcon title='Med oppgaver' />}
+                        overskrift='Med oppgaver'
+                        undertekst='Inntektsmelding 53 st, Lønnstilskudd 3 st ...' />
                 </div>
-                <Link className='innsynisak-lenke' to={{
-                    pathname: 'saksoversikt',
-                    search: location.search,
-                }} onClick={() => {
-                    scroll(0, 0);
-                    loggNavigasjon('saksoversikt', 'se alle saker', location.pathname);
-                }}>
-                    <div className='innsynisak__se-alle-saker'>
-                        <FileFolder />
-                        <BodyShort> Se alle
-                            saker {antallSakerForAlleBedrifter !== undefined ? `(${antallSakerForAlleBedrifter})` : null}</BodyShort>
-                    </div>
-                </Link>
             </div>
-
-            <SaksListe saker={data?.saker.saker} />
-        </div>
+        </>
     );
 };
 
