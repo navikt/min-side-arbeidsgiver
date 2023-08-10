cefebcb138b26119e91ddaa642e9b45a1f5ba0fd
Ken Gullaksen <ken.gullaksen@nav.no>
2023-08-10 15:36:19 +0200

    remotestorage håndterer nå konflikt og flere states

--- src/App/hooks/useStorage.ts
+++ src/App/hooks/useStorage.ts
@@ -1,6 +1,5 @@
-import {useEffect, useState} from "react";
+import {useState} from "react";
 import * as Sentry from "@sentry/browser";
-import {deleteStorage, getStorage, putStorage, StorageItem} from "../../api/dnaApi";
 
 export type UseStorage<S> = [
     S, /* value */
@@ -20,56 +19,11 @@ export const useSessionStorage = <S>(
 ): UseStorage<S> =>
     useStorage(window.sessionStorage, key, initialValue)
 
-export const useRemoteStorage = <S>(
-    key: string,
-    initialValue: S,
-    parser: (value: any) => S,
-): UseStorage<S> => {
-
-
-    const [storedValue, setStoredValue] = useState<S>(initialValue);
-    const [storageItem, setStorageItem] = useState<StorageItem | null>(null);
-
-
-    async function getAndSetStorageItemFromApi() {
-        try {
-            const item = await getStorage(key);
-            setStorageItem(item);
-            setStoredValue(parser(item.data));
-        } catch (error) {
-            console.error(error);
-            Sentry.captureException(error);
-        }
-    }
-
-    useEffect(() => {
-        getAndSetStorageItemFromApi()
-    }, []);
-
-    const setValue = async (value: S) => {
-        try {
-            const valueToStore = value instanceof Function ? value(storedValue) : value;
-            await putStorage(key, valueToStore, storageItem?.version)
-            await getAndSetStorageItemFromApi()
-        } catch (error) {
-            Sentry.captureException(error);
-        }
-    };
-
-    const deleteValue = async () => {
-        await deleteStorage(key, storageItem?.version)
-        setStorageItem(null)
-        setStoredValue(initialValue);
-    }
-
-    return [storedValue, setValue, deleteValue];
-}
-
 function useStorage<S>(
     storage: Storage,
     key: string,
     initialValue: S | ((v: S) => S)
-): UseStorage<S>  {
+): UseStorage<S> {
     const [storedValue, setStoredValue] = useState<S>(() => {
         try {
             const item = storage.getItem(key);
