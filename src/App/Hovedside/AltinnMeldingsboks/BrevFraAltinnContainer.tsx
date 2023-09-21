f93488671ea5f678b6881edecdb78633224c4479
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2023-09-21 10:53:14 +0200

    Kryss-ut for info om fjernede tiltaksbrev

--- src/App/Hovedside/AltinnMeldingsboks/BrevFraAltinnContainer.tsx
+++ src/App/Hovedside/AltinnMeldingsboks/BrevFraAltinnContainer.tsx
@@ -1,13 +1,19 @@
 import React, { useContext } from 'react';
 import { OrganisasjonsDetaljerContext } from '../../OrganisasjonDetaljerProvider';
 import { Alert, Heading, BodyLong } from '@navikt/ds-react';
+import { useLocalStorage } from '../../hooks/useStorage';
 
 const BrevFraAltinnContainer: React.FunctionComponent = (_) => {
     const { valgtOrganisasjon } = useContext(OrganisasjonsDetaljerContext);
+    const [kryssetUt, settKryssetUt] = useLocalStorage('tilskuddsbrev-borte-krysset-ut', false);
 
-    if (valgtOrganisasjon !== undefined && valgtOrganisasjon.altinntilgang.tilskuddsbrev) {
+    if (
+        valgtOrganisasjon !== undefined &&
+        valgtOrganisasjon.altinntilgang.tilskuddsbrev &&
+        !kryssetUt
+    ) {
         return (
-            <Alert variant="info">
+            <Alert variant="info" closeButton onClose={() => settKryssetUt(true)}>
                 <Heading spacing size="small" level="3">
                     Hvor er tilskuddsbrevene?
                 </Heading>
