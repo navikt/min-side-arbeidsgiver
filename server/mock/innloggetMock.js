7d66ce11a4fcaa8d06546cfc1961897fd6b396f6
ebelegu <egzon.belegu@nav.no>
2023-10-10 17:18:28 +0200

    Fjernet fakedings for lokal

--- server/mock/innloggetMock.js
+++ server/mock/innloggetMock.js
@@ -1,26 +1,8 @@
-const sessions = {};
 export const mock = function (app) {
     app.get('/min-side-arbeidsgiver/api/innlogget', (req, res) => {
-        const token = sessions[req.ip];
-        if (token) {
-            console.log('innlogget? ja (session eksisterer)');
-            res.status(200).send();
-        } else {
-            console.log('innlogget? nei (session mangler)');
-            res.status(401).send();
-        }
+        res.status(200).send();
     });
     app.get('/min-side-arbeidsgiver/redirect-til-login', async (req, res) => {
-        const response = await fetch('https://fakedings.intern.dev.nav.no/fake/custom', {
-            method: 'POST',
-            headers: {
-                'Content-type': 'application/x-www-form-urlencoded',
-            },
-            body: `sub=00112233445&aud=${encodeURIComponent('bruker-api')}&acr=Level4`,
-        });
-        const token = await response.text();
-        sessions[req.ip] = token;
-        console.log(`login: setter session til ${token}`);
         res.redirect(req.get('referer'));
     });
 };
