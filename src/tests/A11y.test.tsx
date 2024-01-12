32afd12ab1c752443ab22ad78f070aab84d11903
ebelegu <egzon.belegu@nav.no>
2024-01-12 18:50:41 +0100

    Setter valgt organisasjon for test

--- src/tests/A11y.test.tsx
+++ src/tests/A11y.test.tsx
@@ -1,23 +1,29 @@
-import React, { FC, ReactNode } from 'react';
+import React, { FC, ReactNode, useContext, useEffect } from 'react';
 import { getByText, render, waitFor } from '@testing-library/react';
 import { axe } from 'jest-axe';
 import '@testing-library/jest-dom';
 import Hovedside from '../Pages/Hovedside/Hovedside';
 import { SWRConfig } from 'swr';
 import { AlertsProvider } from '../Pages/Alerts';
-import { OrganisasjonerOgTilgangerProvider } from '../Pages/OrganisasjonerOgTilgangerProvider';
-import { OrganisasjonsDetaljerProvider } from '../Pages/OrganisasjonDetaljerProvider';
-import { basename } from '../paths';
+import {
+    OrganisasjonerOgTilgangerContext,
+    OrganisasjonerOgTilgangerProvider,
+} from '../Pages/OrganisasjonerOgTilgangerProvider';
+import {
+    OrganisasjonsDetaljerContext,
+    OrganisasjonsDetaljerProvider,
+} from '../Pages/OrganisasjonDetaljerProvider';
 import { NotifikasjonWidgetProvider } from '@navikt/arbeidsgiver-notifikasjon-widget';
 import { MemoryRouter } from 'react-router-dom';
 
 describe('Hovedside', () => {
     it('Bruker med alle tilganger får ikke a11y feil', async () => {
         const { container } = render(
-            <TestWrapper>
+            <ComponentTestEnabler>
                 <Hovedside />
-            </TestWrapper>
+            </ComponentTestEnabler>
         );
+        await new Promise((r) => setTimeout(r, 3000));
         await waitFor(() => {
             expect(
                 getByText(container, 'Lær om tilganger og varsler i Altinn')
@@ -28,18 +34,30 @@ describe('Hovedside', () => {
     });
 });
 
-const TestWrapper: FC<{ children: ReactNode }> = ({ children }) => {
+const MedValgtOrganisasjon: FC<{ children: ReactNode }> = ({ children }) => {
+    const { organisasjoner } = useContext(OrganisasjonerOgTilgangerContext);
+    const { valgtOrganisasjon, endreOrganisasjon } = useContext(OrganisasjonsDetaljerContext);
+
+    useEffect(() => {
+        if (valgtOrganisasjon !== undefined) return;
+        endreOrganisasjon(organisasjoner['182345674'].organisasjon);
+    }, [organisasjoner]);
+
+    return valgtOrganisasjon !== undefined ? children : null;
+};
+
+const ComponentTestEnabler: FC<{ children: ReactNode }> = ({ children }) => {
     return (
         <MemoryRouter>
             <NotifikasjonWidgetProvider
                 miljo={'local'}
-                apiUrl={`${basename}/notifikasjon-bruker-api`}
+                apiUrl={`${__BASE_PATH__}/notifikasjon-bruker-api`}
             >
                 <SWRConfig value={{ provider: () => new Map(), dedupingInterval: 0 }}>
                     <AlertsProvider>
                         <OrganisasjonerOgTilgangerProvider>
                             <OrganisasjonsDetaljerProvider>
-                                {children}
+                                <MedValgtOrganisasjon>{children}</MedValgtOrganisasjon>
                             </OrganisasjonsDetaljerProvider>
                         </OrganisasjonerOgTilgangerProvider>
                     </AlertsProvider>
