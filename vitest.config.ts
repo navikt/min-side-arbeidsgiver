5b8a6ab641dcc943f3128b3d0db4e1f3cefd226d
Ken Gullaksen <ken.gullaksen@nav.no>
2024-01-15 18:21:13 +0100

    cleanup vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,9 @@
 /// <reference types="vitest" />
 
 import { configDefaults, defineConfig } from 'vitest/config';
-// import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    //     plugins: [react()],
     define: {
         __BUILD_TIMESTAMP__: new Date(),
         __BASE_PATH__: JSON.stringify('http://localhost/min-side-arbeidsgiver'),
@@ -15,16 +13,6 @@ export default defineConfig({
         environment: 'jsdom',
         exclude: [...configDefaults.exclude, './vitest.setup.ts', 'build/**/*'],
         setupFiles: './vitest.setup.ts',
-        //         coverage: {
-        //             reporter: ['text', 'lcov', 'html'],
-        //             provider: 'v8',
-        //         },
-        //         reporters: ['vitest-sonar-reporter', 'default'],
-        //         outputFile: 'sonar-report.xml',
-        //         css: {
-        //             modules: {
-        //                 classNameStrategy: 'non-scoped',
-        //             },
-        //         },
+        reporters: 'verbose',
     },
 });
