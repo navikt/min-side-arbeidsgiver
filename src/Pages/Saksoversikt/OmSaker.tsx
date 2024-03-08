e4ae8e85b02cee02762092f5546e6d55699c03b6
ebelegu <egzon.belegu@nav.no>
2024-03-08 10:12:12 +0100

    Oppdaterer teksten  i "hva vises her?"

--- src/Pages/Saksoversikt/OmSaker.tsx
+++ src/Pages/Saksoversikt/OmSaker.tsx
@@ -9,9 +9,10 @@ export const OmSaker = forwardRef<HTMLButtonElement, Props>(({ id }, ref) => (
     <HelpText id={id} ref={ref} title="Hva vises her?" aria-label="Hva vises her?">
         Her vises meldinger for:
         <ul style={{ marginTop: '4px' }}>
-            {/*<li>Inntektsmelding for sykepenger</li>*/}
-            <li>Refusjon av sykepenger i arbeidsgiverperioden</li>
+            <li>Inntektsmelding for sykepenger</li>
+            <li>Kandidater til dine stillinger</li>
             <li>Permitteringer, oppsigelser eller innskrenkning i arbeidstid</li>
+            <li>Refusjon av sykepenger i arbeidsgiverperioden</li>
             {/*<li>Yrkesskade (kun meldinger sendt inn etter XXX vises)</li>*/}
         </ul>
         Vi jobber med at flere saker skal vises her etter hvert.
