9057cdc3f4d771825c4d2306a7dc8d06a52917ad
Ken Gullaksen <ken.gullaksen@nav.no>
2024-06-07 10:19:24 +0200

    statisk msw url

--- src/mocks/msw.ts
+++ src/mocks/msw.ts
@@ -5,13 +5,6 @@ import { scenarios } from './scenarios';
 const demoprofil = new URLSearchParams(window.location.search).get('demoprofil') ?? '';
 const demoScenarios = scenarios[demoprofil] ?? [];
 
-console.log({ prod: import.meta.env.PROD });
-
-let mswUrl = `${import.meta.env.BASE_URL}mockServiceWorker.js`;
-if (import.meta.env.MODE === 'demo') {
-    // i demo må msw lastes fra frackend og ikke CDN pga origin policy.
-    mswUrl = '/min-side-arbeidsgiver/mockServiceWorker.js';
-}
 export const startMSW = async () => {
     const { setupWorker } = await import('msw/browser');
     const worker = setupWorker(
@@ -29,7 +22,7 @@ export const startMSW = async () => {
     await worker.start({
         serviceWorker: {
             // https://github.com/mswjs/msw/issues/2055
-            url: mswUrl,
+            url: '/min-side-arbeidsgiver/mockServiceWorker.js',
         },
     });
 };
