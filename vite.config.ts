3723eb424df649e9943b9b1bbf4865763a45d914
Ken Gullaksen <ken.gullaksen@nav.no>
2024-08-21 08:26:36 +0200

    byosbom and parallel deploys

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import legacy from '@vitejs/plugin-legacy';
 import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
