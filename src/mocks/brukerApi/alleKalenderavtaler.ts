996c747450ee5b059931f71fa17bd76f77527b6a
Ken Gullaksen <ken.gullaksen@nav.no>
2024-11-06 14:11:09 +0100

    fiks mock data

--- src/mocks/brukerApi/alleKalenderavtaler.ts
+++ src/mocks/brukerApi/alleKalenderavtaler.ts
@@ -1,10 +1,10 @@
 import { KalenderavtaleTilstand } from '../../api/graphql-types';
 import { fakerNB_NO as faker } from '@faker-js/faker';
-import { dateInFuture, kalenderavtale } from './helpers';
+import { dateInFuture, fakeName, kalenderavtale } from './helpers';
 
 export const alleKalenderavtaler = [
     kalenderavtale({
-        tekst: `Dialogmøte ${faker.person.fullName()}`,
+        tekst: `Dialogmøte ${fakeName()}`,
         startTidspunkt: dateInFuture({ days: 1 }),
         sluttTidspunkt: dateInFuture({ days: 1, hours: 1 }),
         avtaletilstand: KalenderavtaleTilstand.VenterSvarFraArbeidsgiver,
@@ -17,14 +17,14 @@ export const alleKalenderavtaler = [
         merkelapp: 'Dialogmøte',
     }),
     kalenderavtale({
-        tekst: `Dialogmøte ${faker.person.fullName()}`,
+        tekst: `Dialogmøte ${fakeName()}`,
         startTidspunkt: dateInFuture({ days: 3 }),
         avtaletilstand: KalenderavtaleTilstand.ArbeidsgiverHarGodtatt,
         digitalt: true,
         merkelapp: 'Dialogmøte',
     }),
     kalenderavtale({
-        tekst: `Dialogmøte ${faker.person.fullName()}`,
+        tekst: `Dialogmøte ${fakeName()}`,
         startTidspunkt: dateInFuture({ days: 7 }),
         sluttTidspunkt: dateInFuture({ days: 7, hours: 1 }),
         avtaletilstand: KalenderavtaleTilstand.ArbeidsgiverVilEndreTidEllerSted,
@@ -37,13 +37,13 @@ export const alleKalenderavtaler = [
         merkelapp: 'Dialogmøte',
     }),
     kalenderavtale({
-        tekst: `Dialogmøte ${faker.person.fullName()}`,
+        tekst: `Dialogmøte ${fakeName()}`,
         startTidspunkt: dateInFuture({ days: 10 }),
         avtaletilstand: KalenderavtaleTilstand.ArbeidsgiverVilAvlyse,
         merkelapp: 'Dialogmøte',
     }),
     kalenderavtale({
-        tekst: `Dialogmøte ${faker.person.fullName()}`,
+        tekst: `Dialogmøte ${fakeName()}`,
         startTidspunkt: dateInFuture({ months: 1 }),
         sluttTidspunkt: dateInFuture({ months: 1, hours: 3 }),
         avtaletilstand: KalenderavtaleTilstand.Avlyst,
