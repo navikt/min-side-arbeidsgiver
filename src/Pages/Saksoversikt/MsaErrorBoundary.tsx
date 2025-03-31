7ffc9e1b80f98337997eb4baa2d13b81dd7ac1a9
Anders.Vik.Lysne <anders.vik.lysne@nav.no>
2025-03-31 13:39:41 +0200

    Legg til error boundary

---
+++ src/Pages/Saksoversikt/MsaErrorBoundary.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import { Alert } from '@navikt/ds-react';
+import React, { ReactNode } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
+import { SimpleBanner } from '../Banner';
+
+export const MsaErrorBoundary = ({ children }: { children: ReactNode }) => {
+    function onError(error: Error, info: React.ErrorInfo) {
+        console.error(
+            `#MSA: Generisk feil ${error.name}:\nmessage: ${error.message}\ncomponentStack: ${info.componentStack}\n`
+        );
+    }
+
+    return (
+        <ErrorBoundary
+            fallback={
+                <>
+                    <SimpleBanner />
+                    <Alert className={'app-error-alert'} variant={'error'}>
+                        En ukjent feil ble oppdaget. Du kan forsøke å laste inn siden på nytt.{' '}
+                        <div>
+                            <a href={window.location.href}>Last inn siden på nytt</a>
+                        </div>
+                        <div>
+                            <a href={'/'}>Gå til forsiden</a>
+                        </div>
+                    </Alert>
+                </>
+            }
+            onError={onError}
+        >
+            {children}
+        </ErrorBoundary>
+    );
+};
