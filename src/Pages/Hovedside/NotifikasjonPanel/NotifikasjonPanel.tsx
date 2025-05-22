fadcf71fc6cd00716305962b1d6c4840106771f0
Anders.Vik.Lysne <anders.vik.lysne@nav.no>
2025-05-22 12:02:01 +0200

    Gjør hele lenke til saksoversikt klikkbar. legg på hover så den blir litt mer synlig

--- src/Pages/Hovedside/NotifikasjonPanel/NotifikasjonPanel.tsx
+++ src/Pages/Hovedside/NotifikasjonPanel/NotifikasjonPanel.tsx
@@ -269,16 +269,15 @@ const NotifikasjonPanel = () => {
 
             {erUtvidet && (
                 <div className="notifikasjon-utvidet-innhold" role="region">
-                    <div className="notifikasjon-panel-bar">
-                        <InternLenkeMedLogging
-                            loggLenketekst="Saksoversikten lenke i NotifikasjonPanel"
-                            href="/saksoversikt"
-                            ref={søkLinkRef}
-                            onKeyDown={handleSøkKeyDown}
-                        >
+                    <InternLenkeMedLogging
+                        loggLenketekst="Saksoversikten lenke i NotifikasjonPanel"
+                        href="/saksoversikt"
+                        ref={søkLinkRef}
+                        onKeyDown={handleSøkKeyDown}
+                        className="notifikasjon-panel-bar"
+                    >
                             Søk og filtrer på alle saker <ExpandIcon aria-hidden />
-                        </InternLenkeMedLogging>
-                    </div>
+                    </InternLenkeMedLogging>
 
                     <div
                         className="notifikasjon-element-container"
